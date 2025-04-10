1ca50ae5f087190211ddc514e0b02263ec6eb1d2
Prashant Kumar <2018187@iiitdmj.ac.in>
2025-04-10 09:22:50 +0530

    Fibonacci Sequence

---
+++ AZStriverPlaylist/Recursion/FibonacciNumber.java
@@ -0,0 +1,31 @@
+package AZStriverPlaylist.Recursion;
+
+public class FibonacciNumber {
+    
+    public static int nthFibonacciNumber(int n){
+        if(n<=1) return n;
+        return nthFibonacciNumber(n-1)+nthFibonacciNumber(n-2);
+    }
+    public static void main(String[] args) {
+        int input0 = 0;
+        System.out.println("Input : "+input0+" Output : "+FibonacciNumber.nthFibonacciNumber(input0));
+
+        int input1 = 1;
+        System.out.println("Input : "+input1+" Output : "+FibonacciNumber.nthFibonacciNumber(input1));
+
+        int input2 = 2;
+        System.out.println("Input : "+input2+" Output : "+FibonacciNumber.nthFibonacciNumber(input2));
+
+        int input3 = 3;
+        System.out.println("Input : "+input3+" Output : "+FibonacciNumber.nthFibonacciNumber(input3));
+
+        int input4 = 4;
+        System.out.println("Input : "+input4+" Output : "+FibonacciNumber.nthFibonacciNumber(input4));
+
+        int input5 = 5;
+        System.out.println("Input : "+input5+" Output : "+FibonacciNumber.nthFibonacciNumber(input5));
+
+        int input6 = 6;
+        System.out.println("Input : "+input6+" Output : "+FibonacciNumber.nthFibonacciNumber(input6));
+    }
+}
