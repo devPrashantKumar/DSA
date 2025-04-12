e6e7e419d0402aec076669059ff62332b09b37e3
Prashant Kumar <2018187@iiitdmj.ac.in>
2025-04-12 10:05:37 +0530

    missing number and consecutive once

---
+++ AZStriverPlaylist/Array/ConsecutiveOnce.java
@@ -0,0 +1,51 @@
+package AZStriverPlaylist.Array;
+
+import java.util.Arrays;
+
+public class ConsecutiveOnce {
+    /*
+     * Two Pointer / Sliding Window Approach
+     * Time Complexity : O(n)
+     */
+    public static int consecutiveOnceInArrayUsingSlidingWindow(int[] arr){
+        int maxWindow=0;
+        int i=-1;int j=0;
+        for(;j<arr.length;j++){
+            if(arr[j]==0){
+                int window = j-i-1;
+                maxWindow = Math.max(maxWindow, window);
+                i=j;
+            }
+        }
+        maxWindow = Math.max(maxWindow, arr.length-i-1);
+        return maxWindow;
+
+    }
+
+    /*
+     * Time Complexity : O(n)
+     */
+    public static int consecutiveOnceInArray(int[] arr){
+        int counter=0;
+        int maxCounter=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==1){
+                counter++;
+            }
+            else{
+                maxCounter = Math.max(counter,maxCounter);
+                counter=0;
+            }
+        }
+        maxCounter = Math.max(counter,maxCounter);
+        return maxCounter;
+
+    }
+    public static void main(String[] args) {
+        int[] input1 = {1,1,0,1,1,1,0,0,0,1};
+        System.out.println("Input : "+Arrays.toString(input1)+" Output : "+ConsecutiveOnce.consecutiveOnceInArray(input1));
+        System.out.println("Input : "+Arrays.toString(input1)+" Output : "+ConsecutiveOnce.consecutiveOnceInArrayUsingSlidingWindow(input1));
+
+        System.out.println("------------------------------------------------------");
+    }
+}
---
+++ AZStriverPlaylist/Array/MissingNumberInArray.java
@@ -0,0 +1,86 @@
+package AZStriverPlaylist.Array;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class MissingNumberInArray {
+    /*
+     * Time compleity : O(n*2)
+     */
+    public static int missingNumberBruteforce(int[] arr){
+        int len = arr.length;
+        for(int i=1;i<=len;i++){
+            boolean flag=false;
+            for(int j=0;j<len;j++){
+                if(arr[j]==i){
+                    flag=true;
+                    break;
+                }
+            }
+            if(!flag){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /*
+     * Time compleity : O(n)
+     * Space Complexity : O(n)
+     */
+    public static int missingNumberBetter(int[] arr){
+        int len = arr.length;
+        Set<Integer> set = new HashSet<>();
+        for(int i=0;i<len;i++){
+            set.add(arr[i]);
+        }
+        for(int i=1;i<=len;i++){
+            if(!set.contains(i)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /*
+     * Time compleity : O(n)
+     * Space Complexity : O(1)
+     * But there is problem with this approach if number of elements are large then 
+     * there sum can exceed Integer.MAX_Value value, in that case, this solution will fail
+     */
+    public static int missingNumberOptimalUsingSumApproch(int[] arr){
+        int len = arr.length;
+        int sum=0;
+        for(int i=0;i<len;i++){
+            sum +=arr[i];
+        }
+        int sumOfNaturalNumbers = (len+1)*(len+2)/2;
+        return sumOfNaturalNumbers-sum;
+    }
+/*
+     * Time compleity : O(n)
+     * Space Complexity : O(1)
+     * this solution will work fine for large numbers also, because xor of two numbers can not exceed there size
+     */
+    public static int missingNumberOptimalUsingXORApproch(int[] arr){
+        int len = arr.length;
+        int xor=0;
+        for(int i=0;i<len;i++){
+            xor ^= arr[i];
+            xor ^= (i+1);
+        }
+        xor ^= arr.length+1;
+        return xor;
+    }
+
+    public static void main(String[] args) {
+        int[] input1 = {1,4,3,7,6,5};
+        System.out.println("Input : "+Arrays.toString(input1)+" Output : "+MissingNumberInArray.missingNumberBruteforce(input1));
+        System.out.println("Input : "+Arrays.toString(input1)+" Output : "+MissingNumberInArray.missingNumberBetter(input1));
+        System.out.println("Input : "+Arrays.toString(input1)+" Output : "+MissingNumberInArray.missingNumberOptimalUsingSumApproch(input1));
+        System.out.println("Input : "+Arrays.toString(input1)+" Output : "+MissingNumberInArray.missingNumberOptimalUsingXORApproch(input1));
+        System.out.println("------------------------------------------------------");
+
+    }
+}
