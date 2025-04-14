43401fe0daa47728d2827523e8bff3844608730d
Prashant Kumar <2018187@iiitdmj.ac.in>
2025-04-14 09:21:16 +0530

    max subArray len and twoSum problems

---
+++ AZStriverPlaylist/Array/MaxSubarrayOfSumK.java
@@ -0,0 +1,80 @@
+package AZStriverPlaylist.Array;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaxSubarrayOfSumK {
+    /*
+     * Time Complexity : O(n^2)
+     */
+    public static int maxSubArrayBruteforce(int[] arr, int k){
+        int maxLen = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            int sum=0;
+            for(int j=i;j<arr.length;j++){
+                sum += arr[j];
+                if(sum==k){
+                    maxLen = Math.max(maxLen,j-i+1);
+                }
+            } 
+        }
+        return maxLen;
+    }
+
+    /*
+     * Time Complexity : O(n)
+     * Space COmplexity : O(n)
+     */
+    public static int maxSubArrayBetter(int[] arr, int k){
+        int maxLen = Integer.MIN_VALUE;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int sum=0;
+        for(int j=0;j<arr.length;j++){
+            sum += arr[j];
+            if(map.containsKey(sum-k)){
+                int len = j-map.get(sum-k);
+                maxLen = Math.max(maxLen, len);
+            }
+
+            if(!map.containsKey(sum)){
+                map.put(sum, j);
+            }
+        }
+        System.out.println(map);
+        return maxLen;
+    }
+    
+    /*
+     * Time Complexity : O(n)
+     */
+    public static int maxSubArrayOptimal(int[] arr, int k){
+        int maxLen = Integer.MIN_VALUE;
+        int i=0; 
+        int sum=0;
+        for(int j=0;j<arr.length;j++){
+            if(sum<=k){
+                sum += arr[j];
+            } 
+            if(sum==k){
+                maxLen = Math.max(maxLen, j-i+1);
+            }
+            while(sum>k){
+                sum -= arr[i];
+                i++;
+            }
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        int[] input1 = {2,4,1,0, 0 , 0,7,8,3};
+        int k1 = 7;
+        System.out.println("Input : "+Arrays.toString(input1)+" Output : "+MaxSubarrayOfSumK.maxSubArrayBruteforce(input1,k1));
+        System.out.println("Input : "+Arrays.toString(input1)+" Output : "+MaxSubarrayOfSumK.maxSubArrayBetter(input1,k1));
+        System.out.println("Input : "+Arrays.toString(input1)+" Output : "+MaxSubarrayOfSumK.maxSubArrayOptimal(input1,k1));
+        System.out.println("------------------------------------------------------");
+        
+    }
+}
---
+++ AZStriverPlaylist/Array/TwoSum.java
@@ -0,0 +1,81 @@
+package AZStriverPlaylist.Array;
+
+import java.util.*;
+
+class TwoSum {
+    static class ListElement{
+        int value;
+        int index;
+
+        public ListElement(int value,int index){
+            this.value = value;
+            this.index = index;
+        }
+        @Override
+        public String toString() {
+            return "{"+this.index+","+this.value+"}";
+        }
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        List<ListElement>  elementList = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            elementList.add(new ListElement(nums[i],i));
+        }
+        elementList.sort((e1,e2)->{
+            if(e1.value!=e2.value) return e1.value-e2.value;
+            else return e1.index-e2.index;
+        });
+
+        //System.out.println(elementList.toString());
+        int startIndex=0;
+        int endIndex=elementList.size()-1;
+        while(startIndex<endIndex){
+            int sum  = elementList.get(startIndex).value+elementList.get(endIndex).value;
+            if(sum==target) {
+                if(elementList.get(startIndex).index<elementList.get(endIndex).index)
+                return new int[]{elementList.get(startIndex).index,elementList.get(endIndex).index};
+                else return new int[]{elementList.get(endIndex).index,elementList.get(startIndex).index};
+            }
+            else if(sum<target) startIndex++;
+            else endIndex--;
+        }
+        return new int[]{};
+    }
+
+    public static int[] twoSumOptimal(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                return new int[]{map.get(target-nums[i]),i};
+            } 
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = { 3,4,5,6 };
+        int target1 = 7;
+        int[] nums2 = { 4,5,6 };
+        int target2 = 10;
+        int[] nums3 = { -1,-2,-3,-4,-5 };
+        int target3 = -8;
+        System.out.println("---------------------------------------------------");
+        System.out.println("Input : " + Arrays.toString(nums1) +" , "+ target1);
+        System.out.println("Output : " +Arrays.toString(TwoSum.twoSum(nums1,target1)));
+        System.out.println("Output : " +Arrays.toString(TwoSum.twoSumOptimal(nums1,target1)));
+
+        System.out.println("---------------------------------------------------");
+        System.out.println("Input : " + Arrays.toString(nums2) +" , "+ target2);
+        System.out.println("Output : " + Arrays.toString(TwoSum.twoSum(nums2,target2)));
+        System.out.println("Output : " + Arrays.toString(TwoSum.twoSumOptimal(nums2,target2)));
+
+        System.out.println("---------------------------------------------------");
+        System.out.println("Input : " + Arrays.toString(nums3) +" , "+ target3);
+        System.out.println("Output : " + Arrays.toString(TwoSum.twoSum(nums3,target3)));
+        System.out.println("Output : " + Arrays.toString(TwoSum.twoSumOptimal(nums3,target3)));
+
+        System.out.println("---------------------------------------------------");
+    }
+}
