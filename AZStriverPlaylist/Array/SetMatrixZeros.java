65b91a7304362364bfc8766a7b19a4589f1ec5b1
Prashant Kumar <2018187@iiitdmj.ac.in>
2025-05-03 18:40:49 +0530

    set matrix zeroes

--- AZStriverPlaylist/Array/SetMatrixZeros.java
+++ AZStriverPlaylist/Array/SetMatrixZeros.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 public class SetMatrixZeros {
+    /*
+     * Time Complexity : O(n^3)
+     */
     public static int[][] setZeroesSimpleApproch(int[][] matrix){
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[i].length;j++){
@@ -26,6 +29,10 @@ public class SetMatrixZeros {
         return matrix;
     }
     
+    /*
+     * Time Complexity : O(n^2)
+     * space Complexity : O(n)
+     */
     public static int[][] setZeroesBetterApproch(int[][] matrix){
         HashSet<Integer> rows = new HashSet<>();
         HashSet<Integer> columns = new HashSet<>();
@@ -50,11 +57,55 @@ public class SetMatrixZeros {
         }
         return matrix;
     }
+    /*
+     * Time Complexity: O(n^2)
+     */
+    public static int[][] setZeroesOptimalApproch(int[][] matrix){
+        int col0=1;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    if(j!=0){
+                        matrix[0][j]=0;
+                    }
+                    else{
+                        col0 = 0;
+                    }
+                }
+            }
+        }
+
+        for(int i=matrix.length-1;i>0;i--){
+            for(int j=matrix[i].length-1;j>0;j--){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(matrix[0][0]==0){
+            for(int j=matrix[0].length-1;j>0;j--){
+                    matrix[0][j]=0;
+            }
+        }
+
+        if(col0==0){
+            for(int j=matrix.length-1;j>=0;j--){
+                    matrix[j][0]=0;
+            }
+        }
+
+        return matrix;
+    }
     
+    
+
     public static void main(String[] args) {
         int[][] input1 = {{1,1,1},{1,0,1},{1,1,1}};
         System.out.println("Input : "+Arrays.deepToString(input1)+" Output : "+Arrays.deepToString(SetMatrixZeros.setZeroesSimpleApproch(input1)));
         int[][] input12 = {{1,1,1},{1,0,1},{1,1,1}};
         System.out.println("Input : "+Arrays.deepToString(input12)+" Output : "+Arrays.deepToString(SetMatrixZeros.setZeroesBetterApproch(input12)));
+        int[][] input13 = {{1,1,1},{1,0,1},{1,1,1}};
+        System.out.println("Input : "+Arrays.deepToString(input13)+" Output : "+Arrays.deepToString(SetMatrixZeros.setZeroesOptimalApproch(input13)));
     }
 }
