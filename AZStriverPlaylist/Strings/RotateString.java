ebb457d5eec848a2225ec80ef95251afbaa9eeca
Prashant Kumar <77582926+devPrashantKumar@users.noreply.github.com>
2025-05-08 11:29:09 +0530

    Create RotateString.java

---
+++ AZStriverPlaylist/Strings/RotateString.java
@@ -0,0 +1,31 @@
+public class RotateString {
+    public static boolean rotateStringSimpleApproach(String s, String goal) {
+        if(s.length()!=goal.length()) return false;
+        String sTemp = s+s;
+        for(int i=0;i<=sTemp.length()-goal.length();i++){
+            int j=0;
+            for(;j<goal.length();j++){
+                if(sTemp.charAt(i+j)!=goal.charAt(j)) break;
+            }
+            if(j==goal.length()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "abcde";
+        String goal1 = "cdeab";
+        System.out.println("Input : s - "+s1+" , goal - "+goal1);
+        System.out.println("Output : " + RotateString.rotateStringSimpleApproach(s1,goal1));
+        System.out.println("---------------------------------------------------");
+        
+        String s2 = "abcde";
+        String goal2 = "abced";
+        System.out.println("Input : s - "+s2+" , goal - "+goal2);
+        System.out.println("Output : " + RotateString.rotateStringSimpleApproach(s2,goal2));
+        System.out.println("---------------------------------------------------");
+        
+    }
+}
