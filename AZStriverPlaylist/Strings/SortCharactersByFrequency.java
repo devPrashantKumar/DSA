d586da77edad6efe4f1ac717dc3d442f8c05bfb4
Prashant Kumar <2018187@iiitdmj.ac.in>
2025-05-09 12:57:31 +0530

    frequencysort

--- AZStriverPlaylist/Strings/SortCharactersByFrequency.java
+++ AZStriverPlaylist/Strings/SortCharactersByFrequency.java
@@ -3,6 +3,9 @@ package AZStriverPlaylist.Strings;
 import java.util.*;
 
 public class SortCharactersByFrequency {
+    /*
+     * Time complexity : O(nlogn)
+     */
     public static String frequencySort(String s) {
         StringBuilder str = new StringBuilder();
         HashMap<Character, Integer> hmap = new HashMap<>();
@@ -36,12 +39,53 @@ public class SortCharactersByFrequency {
         return str.toString();
     }
 
+    /*
+     * Time conplexity : O(n)
+     * utilising buckets for sorting purpose
+     */
+    public static String frequencySortHashTable(String s) 
+    {
+        StringBuilder sb = new StringBuilder();
+        int[] freq = new int[128];
+        for(char c : s.toCharArray())
+        {
+            freq[c-'0']++;
+        }
+
+        for(int k = 0; k < s.length();)
+        {
+            int max = 0;
+            int ind = 0;
+            for(int j = 0; j < freq.length; j++)
+            {
+                if(freq[j] != 0 && freq[j] > max)
+                {
+                    max = freq[j];
+                    ind = j;
+                }
+            }
+            for(int i = 0; i < max; i++)
+            {
+                sb.append((char)('0'+ind));
+            }
+            freq[ind] = 0;
+            k += max;
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
         String input1 = "tree";
-        System.out.println("Input : "+input1+" Output : "+SortCharactersByFrequency.frequencySort(input1) );
+        System.out.println("Input : "+input1+" Output : "+SortCharactersByFrequency.frequencySort(input1));
+        System.out.println("Input : "+input1+" Output : "+SortCharactersByFrequency.frequencySortHashTable(input1));
+
         String input2 = "cccbbaaaa";
-        System.out.println("Input : "+input2+" Output : "+SortCharactersByFrequency.frequencySort(input2) );
+        System.out.println("Input : "+input2+" Output : "+SortCharactersByFrequency.frequencySort(input2));
+        System.out.println("Input : "+input2+" Output : "+SortCharactersByFrequency.frequencySortHashTable(input2));
+
         String input3 = "ccaabbb";
-        System.out.println("Input : "+input3+" Output : "+SortCharactersByFrequency.frequencySort(input3) );
+        System.out.println("Input : "+input3+" Output : "+SortCharactersByFrequency.frequencySort(input3));
+        System.out.println("Input : "+input3+" Output : "+SortCharactersByFrequency.frequencySortHashTable(input3));
+
     }
 }
