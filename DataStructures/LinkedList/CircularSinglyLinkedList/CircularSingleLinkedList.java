a31464b823947c573a39cfc41af9112269dbc854
Prashant Kumar <77582926+devPrashantKumar@users.noreply.github.com>
2025-05-14 09:42:04 +0530

    refactoring

--- DataStructures/LinkedList/CircularSinglyLinkedList/CircularSingleLinkedList.java
+++ DataStructures/LinkedList/CircularSinglyLinkedList/CircularSingleLinkedList.java
@@ -57,5 +57,18 @@ public class CircularSingleLinkedList {
         System.out.println();
     }
 
-    
+    public static void main(String[] args) {
+        CircularSingleLinkedList circularSingleLinkedList = new CircularSingleLinkedList();
+        circularSingleLinkedList.insertElement(10, 0);
+        circularSingleLinkedList.traveseCircularSingleLinkedList();
+        circularSingleLinkedList.insertElement(11,1);
+        circularSingleLinkedList.traveseCircularSingleLinkedList();
+        circularSingleLinkedList.insertElement(12,0);
+        circularSingleLinkedList.traveseCircularSingleLinkedList();
+        circularSingleLinkedList.insertElement(13,3);
+        circularSingleLinkedList.traveseCircularSingleLinkedList();
+        circularSingleLinkedList.insertElement(14,2);
+        circularSingleLinkedList.traveseCircularSingleLinkedList();
+        circularSingleLinkedList.traveseUsingTailCircularSingleLinkedList();
+    }
 }
