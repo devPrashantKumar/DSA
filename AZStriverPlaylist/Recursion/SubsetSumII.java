988b67f46624817d035668ba893d13dab4405ea5
Prashant Kumar <2018187@iiitdmj.ac.in>
2025-06-18 17:45:05 +0530

    Subset sum 1I

--- AZStriverPlaylist/Recursion/SubsetSumII.java
+++ AZStriverPlaylist/Recursion/SubsetSumII.java
@@ -7,25 +7,30 @@ import java.util.List;
 
 public class SubsetSumII {
     /*
-     * Time Complexity : O(2^n)+(2^n * log 2^n)
-     * (2^n) -> to generate subsets
+     * Time Complexity : O(n * 2^n)
      * (2^n * log 2^n) -> to sort the final subsets sum
-     */ 
+     */
     public static List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        generateSubsets(nums,0,new ArrayList<>(),result);
+        Arrays.sort(nums);
+        generateSubsets(nums, 0, -1, new ArrayList<>(), result);
         return result;
     }
 
-    public static void generateSubsets(int[] nums, int index, List<Integer> set, List<List<Integer>> result) {
-        if(index==nums.length){
+    public static void generateSubsets(int[] nums, int index, int previous,List<Integer> set, List<List<Integer>> result) {
+        if (index == nums.length) {
             result.add(new ArrayList<>(set));
             return;
         }
-        generateSubsets(nums,index+1,set,result);
-        set.add(nums[index]);
-        generateSubsets(nums,index+1,set,result);
-        set.remove(set.size()-1);
+
+        generateSubsets(nums, index + 1,0, set, result);
+
+        if (!(index - 1 >= 0 && nums[index] == nums[index - 1] && previous==0)) {
+            set.add(nums[index]);
+            previous=1;
+            generateSubsets(nums, index + 1,previous, set, result);
+            set.remove(set.size() - 1);
+        }
     }
 
     /*
@@ -36,11 +41,11 @@ public class SubsetSumII {
     public static List<Integer> subsetsUsinBitManipulation(int[] nums) {
         List<Integer> result = new ArrayList<>();
         int numberOfElements = (int) Math.pow(2, nums.length);
-        for(int i=0;i<numberOfElements;i++){
-            int sum=0;
-            for(int j=0;j<nums.length;j++){
-                if((i & (1<<j))!=0){
-                    sum+=nums[j];
+        for (int i = 0; i < numberOfElements; i++) {
+            int sum = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if ((i & (1 << j)) != 0) {
+                    sum += nums[j];
                 }
             }
             result.add(sum);
@@ -50,8 +55,9 @@ public class SubsetSumII {
     }
 
     public static void main(String[] args) {
-        int[] nums1 = {1,2,2};
-        System.out.println("Input 1 " + Arrays.toString(nums1)+" Output : "+SubsetSumII.subsets(nums1));
-        System.out.println("Input 1 " + Arrays.toString(nums1)+" Output : "+SubsetSumI.subsetsUsinBitManipulation(nums1));
+        int[] nums1 = { 1, 2, 2 };
+        System.out.println("Input 1 " + Arrays.toString(nums1) + " Output : " + SubsetSumII.subsets(nums1));
+        System.out.println(
+                "Input 1 " + Arrays.toString(nums1) + " Output : " + SubsetSumI.subsetsUsinBitManipulation(nums1));
     }
 }
