c311420265008660d5cbe06ef46fcb863e21e1d0
Prashant Kumar <2018187@iiitdmj.ac.in>
2025-06-20 12:03:32 +0530

    palindrom partitioning

--- AZStriverPlaylist/Recursion/PalindromePartitioning.java
+++ AZStriverPlaylist/Recursion/PalindromePartitioning.java
@@ -6,27 +6,30 @@ import java.util.List;
 public class PalindromePartitioning {
     public static List<List<String>> partition(String str){
         List<List<String>> result = new ArrayList<>();
-        PalindromePartitioning.partitionUtil(str, 0,new ArrayList<>(), result);
+        partitionUtil(str, 0,new ArrayList<>(), result);
         return result;
     }
 
-    public static void partitionUtil(String str, int index, List<String> partitionList, List<List<String>> result){
+    /*
+     * using StringBuilder instead of String
+     */
+    public static void partitionUtil(String str, int index, List<StringBuilder> partitionList, List<List<String>> result){
         if(index==str.length()){
-            result.add(new ArrayList<>(partitionList));
+            result.add(new ArrayList<>(partitionList.stream().map(StringBuilder::toString).toList()));
             return;
         }
-        String strTemp = "";
+        StringBuilder strTemp = new StringBuilder();
         for(int i=index;i<str.length();i++){
-            strTemp += str.charAt(i);
+            strTemp.append(str.charAt(i));
             if(checkPalindrom(strTemp, 0, strTemp.length()-1)){
                 partitionList.add(strTemp);
-                PalindromePartitioning.partitionUtil(str, i+1, partitionList, result);
+                partitionUtil(str, i+1, partitionList, result);
                 partitionList.remove(partitionList.size()-1);
             }
         }
     }
 
-    public static boolean checkPalindrom(String str, int left, int right){
+    public static boolean checkPalindrom(StringBuilder str, int left, int right){
         if(left>=right) return true;
         if(str.charAt(left)!=str.charAt(right)) return false;
         return checkPalindrom(str, ++left, --right);
