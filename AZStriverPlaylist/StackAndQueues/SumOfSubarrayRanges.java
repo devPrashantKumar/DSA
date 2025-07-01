9dcd884951078cb3eab7b332ae505eade28f0c65
Prashant Kumar <2018187@iiitdmj.ac.in>
2025-07-01 16:50:37 +0530

    sum of subarray ranges

--- AZStriverPlaylist/StackAndQueues/SumOfSubarrayRanges.java
+++ AZStriverPlaylist/StackAndQueues/SumOfSubarrayRanges.java
@@ -5,20 +5,34 @@ import java.util.Stack;
 
 public class SumOfSubarrayRanges {
 
+    /*
+     * TIme Complexity : O(n)
+     */
     public static long subArrayRanges(int[] nums) {
         int[] nextSmallest = nextSmallest(nums);
         int[] nextGreatest = nextGreatest(nums);
         int[] prevSmallestOrEqual = prevSmallestOrEqual(nums);
         int[] prevGreatestOrEqual = prevGreatestOrEqual(nums);
-        System.out.println(Arrays.toString(nextSmallest));
-        System.out.println(Arrays.toString(nextGreatest));
-        System.out.println(Arrays.toString(prevSmallestOrEqual));
-        System.out.println(Arrays.toString(prevGreatestOrEqual));
+        // System.out.println(Arrays.toString(nextSmallest));
+        // System.out.println(Arrays.toString(nextGreatest));
+        // System.out.println(Arrays.toString(prevSmallestOrEqual));
+        // System.out.println(Arrays.toString(prevGreatestOrEqual));
         long smallest = 0;
         long greatest = 0;
         for(int i=0;i<nums.length;i++){
-            smallest = smallest + (((nextSmallest[i]-i)*(i-prevSmallestOrEqual[i]))*nums[i]);
-            greatest = greatest + (((nextGreatest[i]-i)*(i-prevGreatestOrEqual[i]))*nums[i]);
+            // this will not work because we are doing arithmatic operation with int so product will overflow
+            // This line can overflow before the cast to long, because all operands are of type int,
+            // and multiplication of large int values can easily exceed the int range (2^31 - 1 = 2,147,483,647),
+            // causing undefined overflow behavior in Java.
+            // long min = (long)(((nextSmallest[i]-i)*(i-prevSmallestOrEqual[i]))*nums[i]);
+            // long max = (long)(((nextGreatest[i]-i)*(i-prevGreatestOrEqual[i]))*nums[i]);
+
+            // this will work because we are doing arithmatic operation with long so product will not overflow
+            // When an expression includes even one long operand, all subsequent operations are promoted to long.
+            long min = (long)((1L*(nextSmallest[i]-i)*(i-prevSmallestOrEqual[i]))*nums[i]);
+            long max = (long)((1L*(nextGreatest[i]-i)*(i-prevGreatestOrEqual[i]))*nums[i]);
+            smallest = smallest + min;
+            greatest = greatest + max;
         }
         return greatest-smallest;
     }
