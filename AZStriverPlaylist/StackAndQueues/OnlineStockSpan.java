97eedbcf769e2ce3705ae31222857061651bea47
Prashant Kumar <2018187@iiitdmj.ac.in>
2025-07-03 16:06:13 +0530

    online stock span

--- AZStriverPlaylist/StackAndQueues/OnlineStockSpan.java
+++ AZStriverPlaylist/StackAndQueues/OnlineStockSpan.java
@@ -1,36 +1,42 @@
 package AZStriverPlaylist.StackAndQueues;
 
-import java.util.Arrays;
 import java.util.Stack;
 
 public class OnlineStockSpan {
 
     Stack<Integer> stk1;
     Stack<Integer> stk2;
+    int index;
     public OnlineStockSpan() {
         stk1 = new Stack<>();
         stk2 = new Stack<>();
-        
+        index=-1;
     }
     
+    // this approch is also correct ....
+    // public int next(int price) {
+    //     int count=1;
+    //     while(!stk1.isEmpty() && stk1.peek()<=price){
+    //         stk1.pop();
+    //         count += stk2.pop();
+    //     }
+    //     stk1.push(price);
+    //     stk2.push(count);
+    //     return count; 
+    // }
+
     public int next(int price) {
-        int count=1;
+        index++;
         while(!stk1.isEmpty() && stk1.peek()<=price){
             stk1.pop();
-            count += stk2.pop();
+            stk2.pop();
         }
+        int ans  = index - (stk2.isEmpty() ? -1 : stk2.peek());
         stk1.push(price);
-        stk2.push(count);
-        return count;
-        
+        stk2.push(index);
+        return ans;
     }
     public static void main(String[] args) {
-        int[] nums1 = { 0,1,0,2,1,0,1,3,2,1,2,1 };
-        System.out.println("Input : " + Arrays.toString(nums1));
-        System.out.println("output : " + TrappingRainWater.trap(nums1));
-
-        int[] nums12 = { 4,2,0,3,2,5 };
-        System.out.println("Input : " + Arrays.toString(nums12));
-        System.out.println("output : " + TrappingRainWater.trap(nums12));
+        
     }
 }
