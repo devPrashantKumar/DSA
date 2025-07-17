9ef38afa39939fe0c8ec3744608a930ebd85d056
Prashant Kumar <2018187@iiitdmj.ac.in>
2025-07-17 16:30:51 +0530

    optimised ans

--- AZStriverPlaylist/SlidingWindowAndTwoPointers/MaximumPointsYouCanObtainFromCards.java
+++ AZStriverPlaylist/SlidingWindowAndTwoPointers/MaximumPointsYouCanObtainFromCards.java
@@ -21,19 +21,44 @@ public class MaximumPointsYouCanObtainFromCards {
     }
 
 
+    /*
+     * we need to remove k cards from start and end so that removed cards some is maximum,
+     * instead of maximizing that we can minimize the sum of n-k cards in window
+     */
+    public static int maxScoreUsingSlidingWindow(int[] cardPoints, int k) {
+        int ans=Integer.MAX_VALUE;
+        int totalPoints = Arrays.stream(cardPoints).sum();
+        int n = cardPoints.length;
+        int windowSize = n-k;
+        if(windowSize<=0) return totalPoints;
+        int windowPoints=0;
+        for(int i=0;i<windowSize;i++){
+            windowPoints+= cardPoints[i];
+        }
+        ans = Math.min(ans, windowPoints);
+        int l=0;
+        for(int i=windowSize;i<n;i++){
+            windowPoints+= cardPoints[i];
+            windowPoints -= cardPoints[l++];
+            ans = Math.min(ans, windowPoints);
+        }
+        return totalPoints-ans;
+    }
+
+
     public static void main(String[] args) {
         int[] nums1 = { 1,2,3,4,5,6,1 };
         int k1 = 3;
         System.out.println("Input : " + Arrays.toString(nums1));
         System.out.println("output : " + MaximumPointsYouCanObtainFromCards.maxScore(nums1, k1));
-        System.out.println("output : " + MaximumPointsYouCanObtainFromCards.maxScore(nums1, k1));
+        System.out.println("output : " + MaximumPointsYouCanObtainFromCards.maxScoreUsingSlidingWindow(nums1, k1));
 
 
         int[] nums2 = {2,2,2 };
         int k2 = 2;
         System.out.println("Input : " + Arrays.toString(nums2));
         System.out.println("output : " + MaximumPointsYouCanObtainFromCards.maxScore(nums2, k2));
-        System.out.println("output : " + MaximumPointsYouCanObtainFromCards.maxScore(nums2, k2));
+        System.out.println("output : " + MaximumPointsYouCanObtainFromCards.maxScoreUsingSlidingWindow(nums2, k2));
 
 
 
@@ -41,6 +66,6 @@ public class MaximumPointsYouCanObtainFromCards {
         int k3 = 7;
         System.out.println("Input : " + Arrays.toString(nums3));
         System.out.println("output : " + MaximumPointsYouCanObtainFromCards.maxScore(nums3, k3));
-        System.out.println("output : " + MaximumPointsYouCanObtainFromCards.maxScore(nums3, k3));
+        System.out.println("output : " + MaximumPointsYouCanObtainFromCards.maxScoreUsingSlidingWindow(nums3, k3));
     }
 }
