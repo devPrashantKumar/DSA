90cbe2f97010e500bad126a91c318d0c684a8256
Prashant Kumar <77582926+devPrashantKumar@users.noreply.github.com>
2025-07-24 12:02:55 +0530

    Update BubbleSortClass.java

--- AZStriverPlaylist/SortingAlgorithms/BubbleSort/BubbleSortClass.java
+++ AZStriverPlaylist/SortingAlgorithms/BubbleSort/BubbleSortClass.java
@@ -2,7 +2,9 @@ package AZStriverPlaylist.SortingAlgorithms.BubbleSort;
 
 public class BubbleSortClass {
     /*
-     * Time Complexity O(n^2)
+     * Time Complexity 
+     * Best Case : O(n) - only one iteration will happen
+     * Average/Worst Case : O(n^2)
      * Space Complexity O(1)
      * In-Place Algorithm
      * Stable Algorithm
@@ -10,13 +12,16 @@ public class BubbleSortClass {
     public static int[] bubbleSort(int[] arr){
         int n = arr.length;
         for(int i=n-1;i>0;i--){
+            boolean didSwap=false;
             for(int j=0;j<i;j++){
                 if(arr[j]>arr[j+1]){
                     int temp = arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
+                    didSwap=true;
                 }
             }
+            if(!didSwap) break;
         }
         return arr;
     }
