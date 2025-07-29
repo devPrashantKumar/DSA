0479a8da45be5cf3dc0eaa37d404cb4fe4bdcca9
Prashant Kumar <2018187@iiitdmj.ac.in>
2025-07-29 23:04:25 +0530

    graph added

--- AZStriverPlaylist/Graph/AdjacencyMatrixClass.java
+++ AZStriverPlaylist/Graph/AdjacencyMatrixClass.java
@@ -1,4 +1,4 @@
-package DataStructures.Graph;
+package AZStriverPlaylist.Graph;
 
 import java.util.*;
 
@@ -102,6 +102,18 @@ public class AdjacencyMatrixClass {
             }
         }
         return traversalResult;
+    }
+
+    public static void main(String[] args) {
+        AdjacencyMatrixClass adjacencyMatrixClass = new AdjacencyMatrixClass(5);
+        adjacencyMatrixClass.addUndirectedEdge(0, 1);
+        adjacencyMatrixClass.addUndirectedEdge(1, 2);
+        adjacencyMatrixClass.addUndirectedEdge(2, 4);
+        adjacencyMatrixClass.addDirectedEdge(0, 3);
+        adjacencyMatrixClass.printAdjacencyMatrix();
+        System.out.println(Arrays.toString(adjacencyMatrixClass.BFSInAdjacencyMatrix()));
+        System.out.println(Arrays.toString(adjacencyMatrixClass.DFSInAdjacencyMatrix()));
+        System.out.println(Arrays.toString(adjacencyMatrixClass.BFSInAdjacencyMatrix2()));
     }
 
 }
--- AZStriverPlaylist/Graph/WeightedAdjacencyListClass.java
+++ AZStriverPlaylist/Graph/WeightedAdjacencyListClass.java
@@ -1,27 +1,41 @@
-package DataStructures.Graph;
+package AZStriverPlaylist.Graph;
 
 import java.util.*;
 
-public class AdjacencyListClass {
-    int vertices;
-    ArrayList<Integer>[] adjacencyList;
+public class WeightedAdjacencyListClass {
+    class Pair {
+        int vertex;
+        int weight;
 
-    @SuppressWarnings("unchecked")
-    public AdjacencyListClass(int vertices) {
-        this.vertices = vertices;
-        this.adjacencyList = new ArrayList[vertices];
-        for (int i = 0; i < vertices; i++) {
-            adjacencyList[i] = new ArrayList<Integer>();
+        Pair(int vertex, int weight) {
+            this.vertex = vertex;
+            this.weight = weight;
+        }
+
+        public String toString(){
+            return "("+vertex+", "+weight+")";
         }
     }
 
-    public void addUndirectedEdge(int vertex1, int vertex2) {
-        adjacencyList[vertex1].add(vertex2);
-        adjacencyList[vertex2].add(vertex1);
+    int vertices;
+    ArrayList<Pair>[] adjacencyList;
+
+    @SuppressWarnings("unchecked")
+    public WeightedAdjacencyListClass(int vertices) {
+        this.vertices = vertices;
+        this.adjacencyList = new ArrayList[vertices];
+        for (int i = 0; i < vertices; i++) {
+            adjacencyList[i] = new ArrayList<Pair>();
+        }
     }
 
-    public void addDirectedEdge(int from, int to) {
-        adjacencyList[from].add(to);
+    public void addUndirectedEdge(int vertex1, int vertex2, int weight) {
+        adjacencyList[vertex1].add(new Pair(vertex2, weight));
+        adjacencyList[vertex2].add(new Pair(vertex1, weight));
+    }
+
+    public void addDirectedEdge(int from, int to, int weight) {
+        adjacencyList[from].add(new Pair(to, weight));
     }
 
     public void printAdjacencyList() {
@@ -48,9 +62,9 @@ public class AdjacencyListClass {
                     int vertex = queue.poll();
                     traversalResult[k++] = vertex;
                     for (int i = 0; i < adjacencyList[vertex].size(); i++) {
-                        if (visited[adjacencyList[vertex].get(i)] == 0) {
-                            queue.add(adjacencyList[vertex].get(i));
-                            visited[adjacencyList[vertex].get(i)] = 1;
+                        if (visited[adjacencyList[vertex].get(i).vertex] == 0) {
+                            queue.add(adjacencyList[vertex].get(i).vertex);
+                            visited[adjacencyList[vertex].get(i).vertex] = 1;
                         }
                     }
                 }
@@ -72,14 +86,25 @@ public class AdjacencyListClass {
                     int vertex = stk.pop();
                     traversalResult[k++] = vertex;
                     for (int i = 0; i < adjacencyList[vertex].size(); i++) {
-                        if (visited[adjacencyList[vertex].get(i)] == 0) {
-                            stk.add(adjacencyList[vertex].get(i));
-                            visited[adjacencyList[vertex].get(i)] = 1;
+                        if (visited[adjacencyList[vertex].get(i).vertex] == 0) {
+                            stk.add(adjacencyList[vertex].get(i).vertex);
+                            visited[adjacencyList[vertex].get(i).vertex] = 1;
                         }
                     }
                 }
             }
         }
         return traversalResult;
+    }
+
+    public static void main(String[] args) {
+        WeightedAdjacencyListClass weighteddjacencyListClass = new WeightedAdjacencyListClass(5);
+        weighteddjacencyListClass.addUndirectedEdge(0, 1,10);
+        weighteddjacencyListClass.addUndirectedEdge(1, 2,15);
+        weighteddjacencyListClass.addUndirectedEdge(2, 4,5);
+        weighteddjacencyListClass.addDirectedEdge(0, 3,20);
+        weighteddjacencyListClass.printAdjacencyList();
+        System.out.println(Arrays.toString(weighteddjacencyListClass.BFSInAdjacencyList()));
+        System.out.println(Arrays.toString(weighteddjacencyListClass.DFSInAdjacencyList()));
     }
 }
