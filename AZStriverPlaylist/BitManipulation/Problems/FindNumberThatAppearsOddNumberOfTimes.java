28e2e801340defa94384d4f20add0fe82b5f9a48
Prashant Kumar <pk9420663@gmail.com>
2025-08-14 08:13:34 +0530

    odd frequency number

---
+++ AZStriverPlaylist/BitManipulation/Problems/FindNumberThatAppearsOddNumberOfTimes.java
@@ -0,0 +1,27 @@
+package AZStriverPlaylist.BitManipulation.Problems;
+
+import java.util.Arrays;
+
+public class FindNumberThatAppearsOddNumberOfTimes {
+    public static int singleNumber(int[] nums) {
+        int ans = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+
+    public static int singleNumberUsingStreams(int[] nums) {
+        return Arrays.stream(nums).reduce(0, (a, b) -> a ^ b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Odd Frquency Number : " + singleNumber(new int[] {2,2,1}));
+        System.out.println("Odd Frquency Number : " + singleNumber(new int[] {4,1,2,1,2}));
+        System.out.println("Odd Frquency Number : " + singleNumber(new int[] {1}));
+        System.out.println("-------------------------------------------------");
+        System.out.println("Odd Frquency Number : " + singleNumberUsingStreams(new int[] {2,2,1}));
+        System.out.println("Odd Frquency Number : " + singleNumberUsingStreams(new int[] {4,1,2,1,2}));
+        System.out.println("Odd Frquency Number : " + singleNumberUsingStreams(new int[] {1}));
+    }
+}
