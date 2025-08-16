857d907c752a697fd66ce6f16bab7f23b40f7385
Prashant Kumar <pk9420663@gmail.com>
2025-08-16 22:29:24 +0530

    Jump Game solved

--- AZStriverPlaylist/Greedy/JumpGame.java
+++ AZStriverPlaylist/Greedy/JumpGame.java
@@ -3,6 +3,9 @@ package AZStriverPlaylist.Greedy;
 import java.util.Arrays;
 
 public class JumpGame {
+    /*
+     * Time Complexity : O(2*n)
+     */
     public static boolean canJump(int[] nums) {
         l1 : for(int i=0;i<nums.length-1;i++){
             if(nums[i]==0){
@@ -17,9 +20,45 @@ public class JumpGame {
         return true;
     }
 
+    /*
+     * Time Complexity : O(n)
+     */
+    public static boolean canJump2(int[] nums) {
+        int reach = nums[0];
+        for(int i=1;(i<=reach && i<nums.length-1);i++){
+            if(i+nums[i]>reach){
+                reach = i+nums[i];
+            }
+        }
+        if(reach==nums.length-1) return true;
+        return false;
+    }
+
+    /*
+     * Time Complexity : O(n)
+     */
+    public static boolean canJump3(int[] nums) {
+        int reach = 0;
+        int i=0;
+
+        for(;(i<=reach && i<nums.length);i++){
+            if(i==0) reach=nums[0];
+            else if(i+nums[i]>reach){
+                reach = i+nums[i];
+            }
+        }
+        if(i==nums.length) return true;
+        return false;
+    }
+
     public static void main(String[] args) {
         System.out.println("Jump for "+Arrays.toString(new int[]{2,3,1,1,4})+" is possible : "+canJump(new int[]{2,3,1,1,4}));
         System.out.println("Jump for "+Arrays.toString(new int[]{3,2,1,0,4})+" is possible : "+canJump(new int[]{3,2,1,0,4}));
 
+        System.out.println("Jump for "+Arrays.toString(new int[]{2,3,1,1,4})+" is possible : "+canJump2(new int[]{2,3,1,1,4}));
+        System.out.println("Jump for "+Arrays.toString(new int[]{3,2,1,0,4})+" is possible : "+canJump2(new int[]{3,2,1,0,4}));
+
+        System.out.println("Jump for "+Arrays.toString(new int[]{2,3,1,1,4})+" is possible : "+canJump2(new int[]{2,3,1,1,4}));
+        System.out.println("Jump for "+Arrays.toString(new int[]{3,2,1,0,4})+" is possible : "+canJump2(new int[]{3,2,1,0,4}));
     }
 }
