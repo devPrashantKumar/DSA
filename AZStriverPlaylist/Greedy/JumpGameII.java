acf219b801e6ae6f877b93874c41e9a926852dfb
Prashant Kumar <pk9420663@gmail.com>
2025-08-18 16:28:25 +0530

    jump2 added

--- AZStriverPlaylist/Greedy/JumpGameII.java
+++ AZStriverPlaylist/Greedy/JumpGameII.java
@@ -35,6 +35,26 @@ public class JumpGameII {
         }
     }
 
+    public static int jump3(int[] nums) {
+    if (nums.length <= 1) return 0;
+
+    int jumps = 0;
+    int l = 0, r = 0;
+
+    while (r < nums.length - 1) {
+        int farthest = 0;
+        for (int i = l; i <= r; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+        }
+        if (farthest == r) return -1; // stuck
+        l = r + 1;
+        r = farthest;
+        jumps++;
+    }
+    return jumps;
+}
+
+
     public static void main(String[] args) {
         System.out.println("Jump for "+Arrays.toString(new int[]{2,3,1,1,4})+" min jump : "+jump(new int[]{2,3,1,1,4}));
         System.out.println("Jump for "+Arrays.toString(new int[]{3,2,1,0,4})+" min jump : "+jump(new int[]{3,2,1,0,4}));
@@ -42,7 +62,7 @@ public class JumpGameII {
         System.out.println("Jump for "+Arrays.toString(new int[]{2,3,1,1,4})+" min jump : "+jump2(new int[]{2,3,1,1,4}));
         System.out.println("Jump for "+Arrays.toString(new int[]{3,2,1,0,4})+" min jump : "+jump2(new int[]{3,2,1,0,4}));
 
-        System.out.println("Jump for "+Arrays.toString(new int[]{2,3,1,1,4})+" min jump : "+jump(new int[]{2,3,1,1,4}));
-        System.out.println("Jump for "+Arrays.toString(new int[]{3,2,1,0,4})+" min jump : "+jump(new int[]{3,2,1,0,4}));
+        System.out.println("Jump for "+Arrays.toString(new int[]{2,3,1,1,4})+" min jump : "+jump3(new int[]{2,3,1,1,4}));
+        System.out.println("Jump for "+Arrays.toString(new int[]{3,2,1,0,4})+" min jump : "+jump3(new int[]{3,2,1,0,4}));
     }
 }
