84bc4cf05209dd673112b34f40df15366c99a98c
Prashant Kumar <pk9420663@gmail.com>
2025-08-18 16:26:29 +0530

    jump game 2 added

--- AZStriverPlaylist/Greedy/JumpGameII.java
+++ AZStriverPlaylist/Greedy/JumpGameII.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 public class JumpGameII {
 
     public static int jump(int[] nums) {
-        return jumpUtil(nums, 0, 0);
+        int steps = jumpUtil(nums, 0, 0);
+        return steps==Integer.MAX_VALUE ? -1 : steps;
     }
 
     public static int jumpUtil(int[] nums, int index, int jumps) {
@@ -16,12 +17,30 @@ public class JumpGameII {
         }
         return minSteps;
     }
+
+    public static int jump2(int[] nums) {
+        int l=0,r=0;
+        int newR = r;
+        int jumps=0;
+        if(nums.length<=1) return 0;
+        while(true){
+            for(int i=l;i<=r;i++){
+                if(i+nums[i]>newR) newR = i+nums[i];
+            }
+            jumps++;
+            if(newR>=nums.length-1) return jumps;
+            if(newR==r) return -1;
+            l=r+1;
+            r = newR;
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Jump for "+Arrays.toString(new int[]{2,3,1,1,4})+" min jump : "+jump(new int[]{2,3,1,1,4}));
         System.out.println("Jump for "+Arrays.toString(new int[]{3,2,1,0,4})+" min jump : "+jump(new int[]{3,2,1,0,4}));
 
-        System.out.println("Jump for "+Arrays.toString(new int[]{2,3,1,1,4})+" min jump : "+jump(new int[]{2,3,1,1,4}));
-        System.out.println("Jump for "+Arrays.toString(new int[]{3,2,1,0,4})+" min jump : "+jump(new int[]{3,2,1,0,4}));
+        System.out.println("Jump for "+Arrays.toString(new int[]{2,3,1,1,4})+" min jump : "+jump2(new int[]{2,3,1,1,4}));
+        System.out.println("Jump for "+Arrays.toString(new int[]{3,2,1,0,4})+" min jump : "+jump2(new int[]{3,2,1,0,4}));
 
         System.out.println("Jump for "+Arrays.toString(new int[]{2,3,1,1,4})+" min jump : "+jump(new int[]{2,3,1,1,4}));
         System.out.println("Jump for "+Arrays.toString(new int[]{3,2,1,0,4})+" min jump : "+jump(new int[]{3,2,1,0,4}));
