3c026f88db1934e9e8c9069c9e4949ac3f279d29
Prashant Kumar <pk9420663@gmail.com>
2025-08-19 17:01:36 +0530

    LRUCache added

---
+++ AZStriverPlaylist/Greedy/LRUCache.java
@@ -0,0 +1,81 @@
+package AZStriverPlaylist.Greedy;
+
+import java.util.*;
+
+public class LRUCache {
+    static class Node {
+        Node prev;
+        int key;
+        int data;
+        Node next;
+
+        Node(int key,int data) {
+            this.key=key;
+            this.data = data;
+        }
+    };
+
+    Map<Integer, Node> map;
+    Node head, tail;
+    int capacity = 0;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+        head = new Node(0,0);
+        tail = new Node(0,0);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        Node node = map.get(key);
+        if (node != null) {
+            removeNode(node);
+            insertAtHead(node);
+            return node.data;
+        }
+        return -1;
+    }
+
+    public void put(int key, int value) {
+        Node node = map.get(key);
+        if (node != null) {
+            node.data = value;
+            removeNode(node);
+            insertAtHead(node);
+        } else if (capacity > 0) {
+            Node newNode = new Node(key,value);
+            map.put(key, newNode);
+            insertAtHead(newNode);
+            capacity--;
+        } else {
+            removeLastNode();
+            Node newNode = new Node(key,value);
+            map.put(key, newNode);
+            insertAtHead(newNode);
+        }
+    }
+
+    public void insertAtHead(Node node) {
+        node.prev = head.next;
+        head.next.prev = node;
+        node.next = head.next;
+        head.next = node;
+    }
+
+    public void removeNode(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    public void removeLastNode(){
+        map.remove(tail.prev.key);
+        tail.prev.prev.next = tail;
+        tail.prev = tail.prev.prev;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
