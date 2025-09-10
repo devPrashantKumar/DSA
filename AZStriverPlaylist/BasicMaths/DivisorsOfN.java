6e9ada2e4f980f1cfd2e5a9e8bc71788c8d28296
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2025-09-10 23:11:52 +0530

    basic maths

--- AZStriverPlaylist/BasicMaths/DivisorsOfN.java
+++ AZStriverPlaylist/BasicMaths/DivisorsOfN.java
@@ -17,7 +17,6 @@ public class DivisorsOfN {
 
     public static void printDivisorsOfN2(int n){
         if(n<=0) return;
-        //for (int i = 1; i <= n / i; i++) { ... } can also use this syntax for sqrt
         for(int i=1;i<=n/i;i++){
             if(n%i==0){
                 System.out.print(i+" ");
