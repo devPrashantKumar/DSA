9eb7e9707a11b4e808f70c45a5ffe2f6d02e4293
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2025-10-06 09:51:11 +0530

    DesignTwitter

--- AZStriverPlaylist/Heap/DesignTwitter.java
+++ AZStriverPlaylist/Heap/DesignTwitter.java
@@ -9,7 +9,7 @@ public class DesignTwitter {
         System.out.println(twitter.getNewsFeed(1));
         twitter.follow(1, 2);
         twitter.postTweet(2, 6);
-        System.out.println(twitter.getNewsFeed(1)); 
+        System.out.println(twitter.getNewsFeed(1));
         twitter.unfollow(1, 2);
         System.out.println(twitter.getNewsFeed(1));
     }
@@ -21,15 +21,15 @@ class Twitter {
         int postId;
         int timeStamp;
 
-        Post(int postId, int timeStamp){
-            this.postId=postId;
-            this.timeStamp=timeStamp;
+        Post(int postId, int timeStamp) {
+            this.postId = postId;
+            this.timeStamp = timeStamp;
         }
     }
 
     Map<Integer, Set<Integer>> followMap;
     Map<Integer, Set<Post>> userPost;
-    int timeStamp=0;
+    int timeStamp = 0;
 
     public Twitter() {
         followMap = new HashMap<>();
@@ -38,32 +38,29 @@ class Twitter {
 
     public void postTweet(int userId, int tweetId) {
         userPost.putIfAbsent(userId, new HashSet<>());
-        userPost.get(userId).add(new Post(tweetId,timeStamp++));
+        userPost.get(userId).add(new Post(tweetId, timeStamp++));
     }
 
     public List<Integer> getNewsFeed(int userId) {
         List<Integer> feed = new ArrayList<>();
-        List<Post> postFeed = new ArrayList<>();
-        PriorityQueue<Post> pq = new PriorityQueue<>((a,b)->b.timeStamp-a.timeStamp);
-        pq.addAll(userPost.getOrDefault(Integer.valueOf(userId),new HashSet<>()));
+        PriorityQueue<Post> pq = new PriorityQueue<>((a, b) -> b.timeStamp - a.timeStamp);
+        pq.addAll(userPost.getOrDefault(Integer.valueOf(userId), new HashSet<>()));
         for (Integer followeeId : followMap.getOrDefault(userId, new HashSet<>())) {
-            pq.addAll(userPost.getOrDefault(followeeId,new HashSet<>()));
+            pq.addAll(userPost.getOrDefault(followeeId, new HashSet<>()));
         }
-        int i=0;
-        while(!pq.isEmpty() && i<10){
+        int i = 0;
+        while (!pq.isEmpty() && i < 10) {
             Post post = pq.poll();
             feed.add(post.postId);
-            postFeed.add(post);
             i++;
         }
-        pq.addAll(postFeed);
         return feed;
     }
 
     public List<Integer> getNewsFeed2(int userId) {
         List<Integer> feed = new ArrayList<>();
         List<Post> postFeed = new ArrayList<>();
-        
+
         return feed;
     }
 
@@ -73,6 +70,8 @@ class Twitter {
     }
 
     public void unfollow(int followerId, int followeeId) {
-        followMap.getOrDefault(followerId,new HashSet<>()).remove(Integer.valueOf(followeeId));
+        if (followMap.containsKey(followerId)) {
+            followMap.get(followerId).remove(followeeId);
+        }
     }
 }
