93afe1fe33d390bc4f19a33cac6fa073e55e81fc
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2025-10-06 13:03:23 +0530

    KthLargestElementInAStream

---
+++ AZStriverPlaylist/Heap/KthLargestElementInAStream.java
@@ -0,0 +1,39 @@
+package AZStriverPlaylist.Heap;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class KthLargestElementInAStream {
+    public static void main(String[] args) {
+        KthLargest kthLargest = new KthLargest(3, new int[] { 4, 5, 8, 2 });
+        System.out.println(kthLargest.add(3)); // return 4
+        System.out.println(kthLargest.add(5)); // return 5
+        System.out.println(kthLargest.add(10)); // return 5
+        System.out.println(kthLargest.add(9)); // return 8
+        System.out.println(kthLargest.add(4)); // return 8
+    }
+}
+
+class KthLargest {
+    Queue<Integer> pq;
+    int k;
+
+    public KthLargest(int k, int[] nums) {
+        pq = new PriorityQueue<>();
+        this.k = k;
+        for (int num : nums) {
+            add(num);
+        }
+
+    }
+
+    public int add(int val) {
+        if (pq.size() < k)
+            pq.add(val);
+        else if (pq.peek() < val) {
+            pq.poll();
+            pq.add(val);
+        }
+        return pq.peek();
+    }
+}
