d29e593231c7cda61366dee82d9bd7a92b170183
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2025-10-06 21:12:37 +0530

    TopKFrequentElements

--- AZStriverPlaylist/Heap/TopKFrequentElements.java
+++ AZStriverPlaylist/Heap/TopKFrequentElements.java
@@ -9,38 +9,70 @@ public class TopKFrequentElements {
      */
     public static int[] topKFrequent(int[] nums, int k) {
         int[] result = new int[k];
-        Map<Integer,Integer> frequency = new HashMap<>();
-        PriorityQueue<Map.Entry<Integer,Integer>> pq = new PriorityQueue<>((a,b)->b.getValue()-a.getValue());
-        for(int num:nums){
-            frequency.put(num,frequency.getOrDefault(num, 0)+1);
+        Map<Integer, Integer> frequency = new HashMap<>();
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        for (int num : nums) {
+            frequency.put(num, frequency.getOrDefault(num, 0) + 1);
         }
-        for (Map.Entry<Integer,Integer> entry : frequency.entrySet()) {
+        for (Map.Entry<Integer, Integer> entry : frequency.entrySet()) {
             pq.offer(entry);
         }
-        for(int i=0;(i<k && !pq.isEmpty());i++){
-            Map.Entry<Integer,Integer> entry = pq.poll();
-            result[i]=entry.getKey();
+        for (int i = 0; (i < k && !pq.isEmpty()); i++) {
+            Map.Entry<Integer, Integer> entry = pq.poll();
+            result[i] = entry.getKey();
+        }
+        return result;
+    }
+
+    /*
+     * Time Complexity :O(n + n log k + k log k) -> O(n log k)
+     * space Complexity : O(k)
+     */
+    public static int[] topKFrequent2(int[] nums, int k) {
+        int[] result = new int[k];
+        Map<Integer, Integer> frequency = new HashMap<>();
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+        for (int num : nums) {
+            frequency.put(num, frequency.getOrDefault(num, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : frequency.entrySet()) {
+            if (pq.size() < k)
+                pq.offer(entry);
+            else if (pq.peek().getValue() < entry.getValue()) {
+                pq.poll();
+                pq.offer(entry);
+            }
+        }
+        for (int i = 0; (i < k && !pq.isEmpty()); i++) {
+            Map.Entry<Integer, Integer> entry = pq.poll();
+            result[k - i - 1] = entry.getKey();
         }
         return result;
     }
 
     public static void main(String[] args) {
-        int[] input1 = {1,1,1,2,2,3};
+        int[] input1 = { 1, 1, 1, 2, 2, 3 };
         int k1 = 2;
-        System.out.println("Input : "+ Arrays.toString(input1)+", "+k1);
-        System.out.println("Output : "+Arrays.toString(topKFrequent(input1, k1)));
+        System.out.println("Input : " + Arrays.toString(input1) + ", " + k1);
+        System.out.println("Output : " + Arrays.toString(topKFrequent(input1, k1)));
+        System.out.println("Output : " + Arrays.toString(topKFrequent2(input1, k1)));
+
         System.out.println("======================================================================");
 
-        int[] input2 = {1};
+        int[] input2 = { 1 };
         int k2 = 1;
-        System.out.println("Input : "+ Arrays.toString(input2)+", "+k2);
-        System.out.println("Output : "+Arrays.toString(topKFrequent(input2, k2)));
+        System.out.println("Input : " + Arrays.toString(input2) + ", " + k2);
+        System.out.println("Output : " + Arrays.toString(topKFrequent(input2, k2)));
+        System.out.println("Output : " + Arrays.toString(topKFrequent2(input2, k2)));
+
         System.out.println("======================================================================");
 
-        int[] input3 = {1,2,1,2,1,2,3,1,3,2};
+        int[] input3 = { 1, 2, 1, 2, 1, 2, 3, 1, 3, 2 };
         int k3 = 2;
-        System.out.println("Input : "+ Arrays.toString(input3)+", "+k3);
-        System.out.println("Output : "+Arrays.toString(topKFrequent(input3, k3)));
+        System.out.println("Input : " + Arrays.toString(input3) + ", " + k3);
+        System.out.println("Output : " + Arrays.toString(topKFrequent(input3, k3)));
+        System.out.println("Output : " + Arrays.toString(topKFrequent2(input3, k3)));
+
         System.out.println("======================================================================");
     }
 }
