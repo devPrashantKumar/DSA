c7b60c7daf941b9a92ef7a7f954095206ca09174
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2025-10-12 10:26:39 +0530

    findContentChildren2

--- AZStriverPlaylist/Greedy/AssignCookies.java
+++ AZStriverPlaylist/Greedy/AssignCookies.java
@@ -16,15 +16,30 @@ public class AssignCookies {
         return i;
     }
 
+    public static int findContentChildren2(int[] greed, int[] cookies) {
+        Arrays.sort(greed);
+        Arrays.sort(cookies);
+
+        int i=greed.length-1,j=cookies.length-1;
+        while(i>=0 && j>=0){
+            if(greed[i]<=cookies[j]) j--;
+            i--;
+        }
+        return cookies.length-(j+1);
+    }
+
     public static void main(String[] args) {
         int[] greed1 = { 1,2,3 };
         int[] cookies1 = { 1,1 };
         System.out.println("Input : " + Arrays.toString(greed1)+" " + Arrays.toString(cookies1));
         System.out.println("output : " + AssignCookies.findContentChildren(greed1,cookies1));
-        
+        System.out.println("output : " + AssignCookies.findContentChildren2(greed1,cookies1));
+
         int[] greed2 = { 1,2 };
         int[] cookies2 = { 1,2,3 };
         System.out.println("Input : " + Arrays.toString(greed2)+" " + Arrays.toString(cookies2));
         System.out.println("output : " + AssignCookies.findContentChildren(greed2,cookies2));
+        System.out.println("output : " + AssignCookies.findContentChildren2(greed2,cookies2));
+
     }
 }
