47a6c07afabbef469f5098fb9fbe55c89e8e3d79
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2025-10-12 15:29:52 +0530

    JumpGame

--- AZStriverPlaylist/Greedy/JumpGame.java
+++ AZStriverPlaylist/Greedy/JumpGame.java
@@ -4,17 +4,18 @@ import java.util.Arrays;
 
 public class JumpGame {
     /*
-     * Time Complexity : O(2*n)
+     * Time Complexity : O(n^2)
      */
     public static boolean canJump(int[] nums) {
-        l1 : for(int i=0;i<nums.length-1;i++){
-            if(nums[i]==0){
-                int j=i-1;
-                while(j>=0){
-                    if(nums[j]>i-j) continue l1;
+        l1: for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == 0) {
+                int j = i - 1;
+                while (j >= 0) {
+                    if (nums[j] > i - j)
+                        continue l1; // found a jump that skips over zero
                     j--;
                 }
-                return false;
+                return false; // no jump can skip over the zero
             }
         }
         return true;
@@ -25,12 +26,13 @@ public class JumpGame {
      */
     public static boolean canJump2(int[] nums) {
         int reach = nums[0];
-        for(int i=1;(i<=reach && i<nums.length-1);i++){
-            if(i+nums[i]>reach){
-                reach = i+nums[i];
+        for (int i = 1; (i <= reach && i < nums.length - 1); i++) {
+            if (i + nums[i] > reach) {
+                reach = i + nums[i];
             }
         }
-        if(reach==nums.length-1) return true;
+        if (reach >= nums.length - 1)
+            return true;
         return false;
     }
 
@@ -39,26 +41,34 @@ public class JumpGame {
      */
     public static boolean canJump3(int[] nums) {
         int reach = 0;
-        int i=0;
+        int i = 0;
 
-        for(;(i<=reach && i<nums.length);i++){
-            if(i==0) reach=nums[0];
-            else if(i+nums[i]>reach){
-                reach = i+nums[i];
+        for (; (i <= reach && i < nums.length); i++) {
+            if (i == 0)
+                reach = nums[0];
+            else if (i + nums[i] > reach) {
+                reach = i + nums[i];
             }
         }
-        if(i==nums.length) return true;
+        if (i == nums.length)
+            return true;
         return false;
     }
 
     public static void main(String[] args) {
-        System.out.println("Jump for "+Arrays.toString(new int[]{2,3,1,1,4})+" is possible : "+canJump(new int[]{2,3,1,1,4}));
-        System.out.println("Jump for "+Arrays.toString(new int[]{3,2,1,0,4})+" is possible : "+canJump(new int[]{3,2,1,0,4}));
+        System.out.println("Jump for " + Arrays.toString(new int[] { 2, 3, 1, 1, 4 }) + " is possible : "
+                + canJump(new int[] { 2, 3, 1, 1, 4 }));
+        System.out.println("Jump for " + Arrays.toString(new int[] { 3, 2, 1, 0, 4 }) + " is possible : "
+                + canJump(new int[] { 3, 2, 1, 0, 4 }));
 
-        System.out.println("Jump for "+Arrays.toString(new int[]{2,3,1,1,4})+" is possible : "+canJump2(new int[]{2,3,1,1,4}));
-        System.out.println("Jump for "+Arrays.toString(new int[]{3,2,1,0,4})+" is possible : "+canJump2(new int[]{3,2,1,0,4}));
+        System.out.println("Jump for " + Arrays.toString(new int[] { 2, 3, 1, 1, 4 }) + " is possible : "
+                + canJump2(new int[] { 2, 3, 1, 1, 4 }));
+        System.out.println("Jump for " + Arrays.toString(new int[] { 3, 2, 1, 0, 4 }) + " is possible : "
+                + canJump2(new int[] { 3, 2, 1, 0, 4 }));
 
-        System.out.println("Jump for "+Arrays.toString(new int[]{2,3,1,1,4})+" is possible : "+canJump2(new int[]{2,3,1,1,4}));
-        System.out.println("Jump for "+Arrays.toString(new int[]{3,2,1,0,4})+" is possible : "+canJump2(new int[]{3,2,1,0,4}));
+        System.out.println("Jump for " + Arrays.toString(new int[] { 2, 3, 1, 1, 4 }) + " is possible : "
+                + canJump2(new int[] { 2, 3, 1, 1, 4 }));
+        System.out.println("Jump for " + Arrays.toString(new int[] { 3, 2, 1, 0, 4 }) + " is possible : "
+                + canJump2(new int[] { 3, 2, 1, 0, 4 }));
     }
 }
