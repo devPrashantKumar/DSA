3dd854abc2497079af9025dd857e5c49efaf51e8
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2025-10-15 16:38:45 +0530

    VerticalOrderTraversalOfABinaryTree

--- AZStriverPlaylist/Tree/Medium/VerticalOrderTraversalOfABinaryTree.java
+++ AZStriverPlaylist/Tree/Medium/VerticalOrderTraversalOfABinaryTree.java
@@ -23,7 +23,7 @@ public class VerticalOrderTraversalOfABinaryTree {
      * Time Complexity : O(n log n)
      * Space Complexity : O(n)
      */
-    public List<List<Integer>> verticalTraversal(BinaryTreeNode root) {
+    public static List<List<Integer>> verticalTraversal(BinaryTreeNode root) {
         List<NodeDetails> nodesList = new ArrayList<>();
         verticalTraversalUtil(root,0,0,nodesList);
         nodesList.sort((a,b)->{
@@ -35,10 +35,57 @@ public class VerticalOrderTraversalOfABinaryTree {
         return new ArrayList<>(grouped.values());
     }
 
-    public void verticalTraversalUtil(BinaryTreeNode root, int row, int column, List<NodeDetails> list) {
+    public static void verticalTraversalUtil(BinaryTreeNode root, int row, int column, List<NodeDetails> list) {
         if(Objects.isNull(root)) return;
         list.add(new NodeDetails(root.data, row, column));
         verticalTraversalUtil(root.left,row+1,column-1,list);
         verticalTraversalUtil(root.right, row+1,column+1, list);
+    }
+
+    public static void main(String[] args) {
+        BinaryTreeNode root1 = new BinaryTreeNode(
+                3,
+                new BinaryTreeNode(9),
+                new BinaryTreeNode(
+                        20,
+                        new BinaryTreeNode(15),
+                        new BinaryTreeNode(7)));
+        System.out.println(verticalTraversal(root1));
+
+        System.out.println("-------------------------------------------------");
+
+        BinaryTreeNode root2 = new BinaryTreeNode(
+            1,
+            new BinaryTreeNode(
+                2,
+                new BinaryTreeNode(4),
+                new BinaryTreeNode(5)
+            ),
+            new BinaryTreeNode(
+                3,
+                new BinaryTreeNode(6),
+                new BinaryTreeNode(7)
+            )
+        );
+        System.out.println(verticalTraversal(root2));
+
+        System.out.println("-------------------------------------------------");
+
+        BinaryTreeNode root3 = new BinaryTreeNode(
+            1,
+            new BinaryTreeNode(
+                2,
+                new BinaryTreeNode(4),
+                new BinaryTreeNode(6)
+            ),
+            new BinaryTreeNode(
+                3,
+                new BinaryTreeNode(5),
+                new BinaryTreeNode(7)
+            )
+        );
+        System.out.println(verticalTraversal(root3));
+
+        System.out.println("-------------------------------------------------");
     }
 }
