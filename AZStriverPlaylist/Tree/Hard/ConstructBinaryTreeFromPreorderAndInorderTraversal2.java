7496d7a199c26813ca24a2724f9859d56d1c8289
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2025-10-21 19:52:34 +0530

    ConstructBinaryTreeFromPreorderAndInorderTraversal2

---
+++ AZStriverPlaylist/Tree/Hard/ConstructBinaryTreeFromPreorderAndInorderTraversal2.java
@@ -0,0 +1,46 @@
+package AZStriverPlaylist.Tree.Hard;
+
+import java.util.*;
+import AZStriverPlaylist.Tree.BinaryTreeNode;
+import AZStriverPlaylist.Tree.BinaryTreeTraversal;
+
+public class ConstructBinaryTreeFromPreorderAndInorderTraversal2 {
+    public static Map<Integer,Integer> extractInorderIndex(int[] inorder){
+        Map<Integer, Integer> inOrderIndex = new HashMap<>();
+        for(int i=0;i<inorder.length;i++){
+            inOrderIndex.put(inorder[i], i);
+        }
+        return inOrderIndex;
+    }
+    public static BinaryTreeNode buildTree(int[] preorder, int[] inorder) {
+        Map<Integer, Integer> inOrderIndex = extractInorderIndex(inorder);
+
+        return buildTreeUtil(preorder, 0, preorder.length-1, inorder, 0, inorder.length-1, inOrderIndex);
+    }
+
+    public static BinaryTreeNode buildTreeUtil(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd, Map<Integer, Integer> inOrderIndex) {
+        if(inStart>inEnd) return null;
+        BinaryTreeNode node = new BinaryTreeNode(preorder[preStart]);
+        int index = inOrderIndex.get(preorder[preStart]);
+        //findPreInInorder(preorder[preStart],inorder,inStart,inEnd);
+        int leftSide = index-inStart;
+        node.left = buildTreeUtil(preorder, preStart+1, preStart+leftSide, inorder, inStart, index-1, inOrderIndex);
+        node.right = buildTreeUtil(preorder,  preStart+leftSide+1, preEnd, inorder, index+1, inEnd, inOrderIndex);
+        return node;
+    }
+
+    public static int findPreInInorder(int pre, int[] inorder, int inStart, int inEnd){
+        for(int i=inStart;i<=inEnd;i++){
+            if(inorder[i]==pre) return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] inorder1 = {9,3,15,20,7};
+        int[] preorder1 = {3,9,20,15,7};
+        BinaryTreeNode root1 = buildTree(preorder1,inorder1);
+        BinaryTreeTraversal.iterativeInorderTraversal(root1);
+        BinaryTreeTraversal.iterativePreOrderTraversal(root1);
+    }
+}
