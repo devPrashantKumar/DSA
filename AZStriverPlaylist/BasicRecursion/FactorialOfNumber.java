0837f50cd6915ab90f1da5a639d44c91cb1e0ebf
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2025-10-21 17:44:58 +0530

    FactorialOfNumber

--- AZStriverPlaylist/BasicRecursion/FactorialOfNumber.java
+++ AZStriverPlaylist/BasicRecursion/FactorialOfNumber.java
@@ -7,15 +7,29 @@ public class FactorialOfNumber {
         return factorialUsingParameterisedApproach(num-1, prod*num);
     }
 
-    // Classic recursive pattern.
+    // Functional (Non-tail Recursion) / Classic recursive pattern.
     public static int factorialUsingFunctionalApproach(int num){
         if(num==0) return 1;
         return num * factorialUsingFunctionalApproach(num-1);
+    }
+
+     // Tail Recursion using a Mutable Reference
+    public static int factorialUsingApproach2(int num){
+        int[] ans  ={1};
+        factorialUsingApproach2Util(num, ans);
+        return ans[0];
+    }
+
+    public static void factorialUsingApproach2Util(int num, int[] ans){
+        if(num==0) return;
+        ans[0]*=num;
+        factorialUsingApproach2Util(num-1, ans);
     }
 
     public static void main(String[] args) {
         int input1=5;
         System.out.println("Parameterized: Factorial of " + input1 + " -> "+FactorialOfNumber.factorialUsingParameterisedApproach(input1, 1));
         System.out.println("Functional: Factorial of " + input1 + " -> "+FactorialOfNumber.factorialUsingFunctionalApproach(input1));
+        System.out.println("Tail Recursion using a Mutable Reference: Factorial of " + input1 + " -> "+FactorialOfNumber.factorialUsingApproach2(input1));
     }
 }
