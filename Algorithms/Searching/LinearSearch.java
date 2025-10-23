03bcbd6159b28b02e1bbcf110d40c2f5142f7b93
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2025-10-23 20:39:57 +0530

    code refactoring

--- Algorithms/Searching/LinearSearch.java
+++ Algorithms/Searching/LinearSearch.java
@@ -13,4 +13,10 @@ public class LinearSearch {
         }
         return null;
     }
+
+    public static void main(String[] args) {
+        Integer[] arr = {1,5,2,9,4,7,3};
+        System.out.println("Element found 3 - "+LinearSearch.linearSearch(arr, 3));
+        System.out.println("Element found 13 - "+LinearSearch.linearSearch(arr, 13));
+    }
 }
