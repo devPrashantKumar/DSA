6c118c23fc8aa16a519207ccbf2ece4a86e49ed2
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2025-10-25 16:06:50 +0530

    FrogJump

--- AZStriverPlaylist/DynamicProgramming/FrogJump.java
+++ AZStriverPlaylist/DynamicProgramming/FrogJump.java
@@ -13,11 +13,34 @@ public class FrogJump {
         return Math.min(oneJump, twoJump);
     }
 
+    public static int frogJumpDP(int[] heights) {
+        Integer[] dp = new Integer[heights.length];
+        dp[heights.length-1]=0;
+        return frogJumpDPUtil(heights,0,heights.length-1,dp);
+    }
+
+    public static int frogJumpDPUtil(int[] heights, int start, int end, Integer[] dp) {
+        if(dp[start]!=null) return dp[start];
+
+        int oneJump = Math.abs(heights[start+1]-heights[start])+frogJumpDPUtil(heights, start+1, end,dp);
+        if(start+2>end) return oneJump;
+        int twoJump = Math.abs(heights[start+2]-heights[start])+frogJumpDPUtil(heights, start+2, end,dp);
+        
+        dp[start] = Math.min(oneJump, twoJump);
+        return dp[start];
+    }
+
+
+
     public static void main(String[] args) {
         System.out.println(frogJump(new int[]{2, 1, 3, 5, 4}));
+        System.out.println(frogJumpDP(new int[]{2, 1, 3, 5, 4}));
+
         System.out.println("----------------------------------------------------");
 
         System.out.println(frogJump(new int[]{7, 5, 1, 2, 6}));
+        System.out.println(frogJumpDP(new int[]{7, 5, 1, 2, 6}));
+
         System.out.println("----------------------------------------------------");
     }
 }
