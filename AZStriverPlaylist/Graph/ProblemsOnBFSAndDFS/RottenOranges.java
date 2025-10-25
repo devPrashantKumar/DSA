5298bcdeff8b7e249343f422b36456c277a21b7d
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2025-10-25 09:43:52 +0530

    RottenOranges

--- AZStriverPlaylist/Graph/ProblemsOnBFSAndDFS/RottenOranges.java
+++ AZStriverPlaylist/Graph/ProblemsOnBFSAndDFS/RottenOranges.java
@@ -1,57 +1,189 @@
 package AZStriverPlaylist.Graph.ProblemsOnBFSAndDFS;
 
-public class RottenOranges {
+import java.util.LinkedList;
+import java.util.Queue;
 
-    /* Bruteforce Approach
+public class RottenOranges {
+    static class Position {
+        int row;
+        int column;
+
+        Position(int row, int column) {
+            this.row = row;
+            this.column = column;
+        }
+    }
+
+    /*
+     * Bruteforce Approach
      * Time Complexity : O((m * n)^2)
      * Space Complexity : O(1)
      */
     public static int orangesRotting(int[][] grid) {
-        int time=-1;
+        int time = -1;
         boolean flag = false;
         boolean freshExists = false;
         int m = grid.length;
-        int n = m>0 ? grid[0].length : 0;
+        int n = m > 0 ? grid[0].length : 0;
 
-        do{
+        do {
             time++;
-            freshExists=false;
+            freshExists = false;
             flag = false;
-            for(int i=0;i<m;i++){
-                for(int j=0;j<n;j++){
-                    if(grid[i][j]==1){
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][j] == 1) {
                         freshExists = true;
-                        if((i-1>=0 && grid[i-1][j]==2) 
-                            || (i+1<=m-1 && grid[i+1][j]==2)
-                            || (j-1>=0 && grid[i][j-1]==2)
-                            || (j+1<=n-1 && grid[i][j+1]==2)){
-                                grid[i][j]=3;
-                                flag=true;
-                            }
+                        if ((i - 1 >= 0 && grid[i - 1][j] == 2)
+                                || (i + 1 <= m - 1 && grid[i + 1][j] == 2)
+                                || (j - 1 >= 0 && grid[i][j - 1] == 2)
+                                || (j + 1 <= n - 1 && grid[i][j + 1] == 2)) {
+                            grid[i][j] = 3;
+                            flag = true;
+                        }
                     }
                 }
             }
 
-            for(int i=0;i<m;i++){
-                for(int j=0;j<n;j++){
-                    if(grid[i][j]==3) grid[i][j]=2;
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][j] == 3)
+                        grid[i][j] = 2;
                 }
             }
-        }while(flag);
+        } while (flag);
         return (freshExists) ? -1 : time;
     }
 
+    /*
+     * Bruteforce Approach
+     * Time Complexity : O((m * n)^2)
+     * Space Complexity : O(1)
+     */
+    public static int orangesRotting2(int[][] grid) {
+        int time = -1;
+        boolean flag = false;
+        boolean freshExists = false;
+        int m = grid.length;
+        int n = m > 0 ? grid[0].length : 0;
+
+        do {
+            time++;
+            freshExists = false;
+            flag = false;
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][j] == 2) {
+                        if (i - 1 >= 0 && grid[i - 1][j] == 1) {
+                            grid[i - 1][j] = 3;
+                            flag = true;
+                        }
+                        if (j + 1 <= n - 1 && grid[i][j + 1] == 1) {
+                            grid[i][j + 1] = 3;
+                            flag = true;
+                        }
+                        if (j - 1 >= 0 && grid[i][j - 1] == 1) {
+                            grid[i][j - 1] = 3;
+                            flag = true;
+                        }
+                        if (i + 1 <= m - 1 && grid[i + 1][j] == 1) {
+                            grid[i + 1][j] = 3;
+                            flag = true;
+                        }
+
+                    }
+                }
+            }
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][j] == 3)
+                        grid[i][j] = 2;
+                    if (grid[i][j] == 1)
+                        freshExists = true;
+                }
+            }
+        } while (flag);
+        return (freshExists) ? -1 : time;
+    }
+
+    /*
+     * Optimised Approach
+     * Time Complexity : O((m * n))
+     * Space Complexity : O(m*n)
+     */
+    public static int orangesRottingUsingBFS(int[][] grid) {
+        int time = -1;
+        int m = grid.length;
+        int n = m > 0 ? grid[0].length : 0;
+        boolean freshExists = false;
+        Queue<Position> queue = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2)
+                    queue.offer(new Position(i, j));
+                if (grid[i][j] == 1)
+                    freshExists = true;
+            }
+        }
+        if(!freshExists) return 0;
+        while (!queue.isEmpty()) {
+            time++;
+            int rotten = queue.size();
+            for (int i = 0; i < rotten; i++) {
+                Position rottenPosition = queue.poll();
+                int row = rottenPosition.row;
+                int column = rottenPosition.column;
+                if (row - 1 >= 0 && grid[row - 1][column] == 1) {
+                    grid[row - 1][column] = 2;
+                    queue.offer(new Position(row - 1, column));
+                }
+                if (column + 1 <= n - 1 && grid[row][column + 1] == 1) {
+                    grid[row][column + 1] = 2;
+                    queue.offer(new Position(row, column + 1));
+                }
+                if (column - 1 >= 0 && grid[row][column - 1] == 1) {
+                    grid[row][column - 1] = 2;
+                    queue.offer(new Position(row, column - 1));
+                }
+                if (row + 1 <= m - 1 && grid[row + 1][column] == 1) {
+                    grid[row + 1][column] = 2;
+                    queue.offer(new Position(row + 1, column));
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1)
+                    return -1;
+            }
+        }
+
+        return time;
+    }
+
     public static void main(String[] args) {
-        int[][] grid1 = {{2,1,1},{1,1,0},{0,1,1}};
-        System.out.println(orangesRotting(grid1));
+        System.out.println(orangesRotting(new int[][] { { 2, 1, 1 }, { 1, 1, 0 }, { 0, 1, 1 } }));
+        System.out.println(orangesRotting2(new int[][] { { 2, 1, 1 }, { 1, 1, 0 }, { 0, 1, 1 } }));
+        System.out.println(orangesRottingUsingBFS(new int[][] { { 2, 1, 1 }, { 1, 1, 0 }, { 0, 1, 1 } }));
+
         System.out.println("---------------------------------------------");
-    
-        int[][] grid2 = {{2,1,1},{0,1,1},{1,0,1}};
-        System.out.println(orangesRotting(grid2));
+        System.out.println(orangesRotting(new int[][] { { 2, 1, 1 }, { 0, 1, 1 }, { 1, 0, 1 } }));
+        System.out.println(orangesRotting2(new int[][] { { 2, 1, 1 }, { 0, 1, 1 }, { 1, 0, 1 } }));
+        System.out.println(orangesRottingUsingBFS(new int[][] { { 2, 1, 1 }, { 0, 1, 1 }, { 1, 0, 1 } }));
+
         System.out.println("---------------------------------------------");
-    
-        int[][] grid3 = {{0,2}};
-        System.out.println(orangesRotting(grid3));
+
+        System.out.println(orangesRotting(new int[][] { { 0, 2 } }));
+        System.out.println(orangesRotting2(new int[][] { { 0, 2 } }));
+        System.out.println(orangesRotting2(new int[][] { { 0, 2 } }));
+
+        System.out.println("---------------------------------------------");
+        System.out.println(orangesRotting(new int[][] { { 0 } }));
+        System.out.println(orangesRotting2(new int[][] { { 0 } }));
+        System.out.println(orangesRotting2(new int[][] { { 0 } }));
+
         System.out.println("---------------------------------------------");
     }
 }
