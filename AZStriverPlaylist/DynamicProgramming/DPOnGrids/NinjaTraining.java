ddfc9296a2b5fc31b62fdf655371723f1c2a636a
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2025-10-28 11:19:27 +0530

    ninjaTrainingDPMemoization

--- AZStriverPlaylist/DynamicProgramming/DPOnGrids/NinjaTraining.java
+++ AZStriverPlaylist/DynamicProgramming/DPOnGrids/NinjaTraining.java
@@ -1,5 +1,7 @@
 package AZStriverPlaylist.DynamicProgramming.DPOnGrids;
 
+import java.util.Arrays;
+
 public class NinjaTraining {
     public static int ninjaTraining(int[][] matrix) {
         return ninjaTrainingUtil(matrix, -1, matrix.length - 1);
@@ -8,25 +10,58 @@ public class NinjaTraining {
     public static int ninjaTrainingUtil(int[][] matrix, int lastUsed, int end) {
         if (end < 0)
             return 0;
-        int zeroSelect=0,oneSelect=0,twoSelect=0;
-        if (lastUsed == -1 || lastUsed == 1 || lastUsed == 2 ) {
+        int zeroSelect = 0, oneSelect = 0, twoSelect = 0;
+        if (lastUsed == -1 || lastUsed == 1 || lastUsed == 2) {
             zeroSelect = matrix[end][0] + ninjaTrainingUtil(matrix, 0, end - 1);
         }
-        if (lastUsed == -1 || lastUsed == 0 || lastUsed == 2 ) {
+        if (lastUsed == -1 || lastUsed == 0 || lastUsed == 2) {
             oneSelect = matrix[end][1] + ninjaTrainingUtil(matrix, 1, end - 1);
         }
-        if (lastUsed == -1 || lastUsed == 0 || lastUsed == 1 ) {
+        if (lastUsed == -1 || lastUsed == 0 || lastUsed == 1) {
             twoSelect = matrix[end][2] + ninjaTrainingUtil(matrix, 2, end - 1);
         }
-        return Math.max(zeroSelect,Math.max(oneSelect, twoSelect));
+        return Math.max(zeroSelect, Math.max(oneSelect, twoSelect));
+    }
+
+    public static int ninjaTrainingDPMemoization(int[][] matrix) {
+        int[][] dp = new int[matrix.length][3];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        return ninjaTrainingDPMemoizationUtil(matrix, -1, matrix.length - 1, dp);
+    }
+
+    public static int ninjaTrainingDPMemoizationUtil(int[][] matrix, int lastUsed, int end, int[][] dp) {
+        if (end < 0)
+            return 0;
+        if (lastUsed != -1 && dp[end][lastUsed] != -1)
+            return dp[end][lastUsed];
+        int zeroSelect = 0, oneSelect = 0, twoSelect = 0;
+        if (lastUsed == -1 || lastUsed == 1 || lastUsed == 2) {
+            zeroSelect = matrix[end][0] + ninjaTrainingDPMemoizationUtil(matrix, 0, end - 1, dp);
+        }
+        if (lastUsed == -1 || lastUsed == 0 || lastUsed == 2) {
+            oneSelect = matrix[end][1] + ninjaTrainingDPMemoizationUtil(matrix, 1, end - 1, dp);
+        }
+        if (lastUsed == -1 || lastUsed == 0 || lastUsed == 1) {
+            twoSelect = matrix[end][2] + ninjaTrainingDPMemoizationUtil(matrix, 2, end - 1, dp);
+        }
+        int result = Math.max(zeroSelect, Math.max(oneSelect, twoSelect));
+        if (lastUsed != -1)
+            dp[end][lastUsed] = result;
+        return result;
     }
 
     public static void main(String[] args) {
-        int[][] matrix1 = {{10, 40, 70},{20,50,80},{30,60,90}};
+        int[][] matrix1 = { { 10, 40, 70 }, { 20, 50, 80 }, { 30, 60, 90 } };
         System.out.println(ninjaTraining(matrix1));
+        System.out.println(ninjaTrainingDPMemoization(matrix1));
+
         System.out.println("================================================");
-        int[][] matrix2 = {{70, 40, 10},{180,20,5},{200,60,30}};
+        int[][] matrix2 = { { 70, 40, 10 }, { 180, 20, 5 }, { 200, 60, 30 } };
         System.out.println(ninjaTraining(matrix2));
+        System.out.println(ninjaTrainingDPMemoization(matrix2));
+
         System.out.println("================================================");
     }
 }
