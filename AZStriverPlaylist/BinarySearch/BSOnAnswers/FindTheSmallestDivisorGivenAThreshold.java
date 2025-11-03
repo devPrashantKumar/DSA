37f1eb7af41eb979c94824e2e87a021ea402de2f
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2025-11-03 20:38:27 +0530

    test added

--- AZStriverPlaylist/BinarySearch/BSOnAnswers/FindTheSmallestDivisorGivenAThreshold.java
+++ AZStriverPlaylist/BinarySearch/BSOnAnswers/FindTheSmallestDivisorGivenAThreshold.java
@@ -1,7 +1,7 @@
 package AZStriverPlaylist.BinarySearch.BSOnAnswers;
 
 public class FindTheSmallestDivisorGivenAThreshold {
-    
+
     public static int smallestDivisor(int[] nums, int threshold) {
         int maxNum = Integer.MIN_VALUE;
         for(int num : nums){
@@ -24,5 +24,10 @@ public class FindTheSmallestDivisorGivenAThreshold {
             if (count>threshold) return false;
         }
         return (count<=threshold);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(smallestDivisor(new int[] {1,2,5,9 }, 6));
+        System.out.println(smallestDivisor(new int[] { 44,22,33,11,1 }, 5));
     }
 }
