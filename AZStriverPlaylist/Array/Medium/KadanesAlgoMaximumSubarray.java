c5f966527e4c5335fac189b62aa989777e998296
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-01-03 18:34:17 +0530

    maxSubarraySumKadanesAlgo3

--- AZStriverPlaylist/Array/Medium/KadanesAlgoMaximumSubarray.java
+++ AZStriverPlaylist/Array/Medium/KadanesAlgoMaximumSubarray.java
@@ -70,23 +70,39 @@ public class KadanesAlgoMaximumSubarray {
         return maxSum;
     }
 
+    public static int maxSubarraySumKadanesAlgo3(int[] arr){
+        int maxSum = Integer.MIN_VALUE;
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum = Math.max(sum+arr[i], arr[i]);
+            maxSum = Math.max(maxSum, sum);
+        }
+        return maxSum;
+    }
+
     public static void main(String[] args) {
         int[] input1 = {-2,1,-3,4,-1,2,1,-5,4};
         System.out.println("Input : "+Arrays.toString(input1)+" Output : "+KadanesAlgoMaximumSubarray.maxSubarraySum(input1));
         System.out.println("Input : "+Arrays.toString(input1)+" Output : "+KadanesAlgoMaximumSubarray.maxSubarraySum2(input1));
         System.out.println("Input : "+Arrays.toString(input1)+" Output : "+KadanesAlgoMaximumSubarray.maxSubarraySumKadanesAlgo(input1));
+        System.out.println("Input : "+Arrays.toString(input1)+" Output : "+KadanesAlgoMaximumSubarray.maxSubarraySumKadanesAlgo2(input1));
+        System.out.println("Input : "+Arrays.toString(input1)+" Output : "+KadanesAlgoMaximumSubarray.maxSubarraySumKadanesAlgo3(input1));
 
         System.out.println("-------------------------------------------");
         int[] input2 = {1};
-        System.out.println("Input : "+Arrays.toString(input1)+" Output : "+KadanesAlgoMaximumSubarray.maxSubarraySum(input2));
-        System.out.println("Input : "+Arrays.toString(input1)+" Output : "+KadanesAlgoMaximumSubarray.maxSubarraySum2(input2));
-        System.out.println("Input : "+Arrays.toString(input1)+" Output : "+KadanesAlgoMaximumSubarray.maxSubarraySumKadanesAlgo(input2));
+        System.out.println("Input : "+Arrays.toString(input2)+" Output : "+KadanesAlgoMaximumSubarray.maxSubarraySum(input2));
+        System.out.println("Input : "+Arrays.toString(input2)+" Output : "+KadanesAlgoMaximumSubarray.maxSubarraySum2(input2));
+        System.out.println("Input : "+Arrays.toString(input2)+" Output : "+KadanesAlgoMaximumSubarray.maxSubarraySumKadanesAlgo(input2));
+        System.out.println("Input : "+Arrays.toString(input2)+" Output : "+KadanesAlgoMaximumSubarray.maxSubarraySumKadanesAlgo2(input2));
+        System.out.println("Input : "+Arrays.toString(input2)+" Output : "+KadanesAlgoMaximumSubarray.maxSubarraySumKadanesAlgo3(input2));
 
         System.out.println("-------------------------------------------");
         int[] input3 = {5,4,-1,7,8};
-        System.out.println("Input : "+Arrays.toString(input1)+" Output : "+KadanesAlgoMaximumSubarray.maxSubarraySum(input3));
-        System.out.println("Input : "+Arrays.toString(input1)+" Output : "+KadanesAlgoMaximumSubarray.maxSubarraySum2(input3));
-        System.out.println("Input : "+Arrays.toString(input1)+" Output : "+KadanesAlgoMaximumSubarray.maxSubarraySumKadanesAlgo(input3));
+        System.out.println("Input : "+Arrays.toString(input3)+" Output : "+KadanesAlgoMaximumSubarray.maxSubarraySum(input3));
+        System.out.println("Input : "+Arrays.toString(input3)+" Output : "+KadanesAlgoMaximumSubarray.maxSubarraySum2(input3));
+        System.out.println("Input : "+Arrays.toString(input3)+" Output : "+KadanesAlgoMaximumSubarray.maxSubarraySumKadanesAlgo(input3));
+        System.out.println("Input : "+Arrays.toString(input3)+" Output : "+KadanesAlgoMaximumSubarray.maxSubarraySumKadanesAlgo2(input3));
+        System.out.println("Input : "+Arrays.toString(input3)+" Output : "+KadanesAlgoMaximumSubarray.maxSubarraySumKadanesAlgo3(input3));
 
         System.out.println("------------------------------------------------------");
         
