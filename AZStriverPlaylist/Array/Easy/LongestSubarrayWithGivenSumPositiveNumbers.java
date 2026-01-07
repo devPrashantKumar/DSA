4e6914816523ee5bec8aeeef76ba19f4c1b42cbc
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-01-07 16:16:06 +0530

    code refactoring

--- AZStriverPlaylist/Array/Easy/LongestSubarrayWithGivenSumPositiveNumbers.java
+++ AZStriverPlaylist/Array/Easy/LongestSubarrayWithGivenSumPositiveNumbers.java
@@ -4,26 +4,52 @@ import java.util.Arrays;
 
 public class LongestSubarrayWithGivenSumPositiveNumbers {
 
-    public static int longestSubarray(int[] nums, int k) {
-        int i=0;
-        int sum=0;
-        int maxAns = 0;
-        for(int j=0;j<nums.length;j++){
-            sum += nums[j];
-            if(sum==k) maxAns = Math.max(maxAns,j-i+1);
-            else if(sum>k) sum-=nums[i++];
+    /*
+     * Time Complexity : O(n^2)
+     */
+    public static int longestSubarrayBruteforce(int[] arr, int k){
+        int maxLen = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            int sum=0;
+            for(int j=i;j<arr.length;j++){
+                sum += arr[j];
+                if(sum==k){
+                    maxLen = Math.max(maxLen,j-i+1);
+                }
+            } 
         }
-       return maxAns;
+        return maxLen;
+    }
+
+    public static int longestSubarray(int[] arr, int k) {
+       int maxLen = 0;
+        int i=0; 
+        int sum=0;
+        for(int j=0;j<arr.length;j++){
+            if(sum<=k){
+                sum += arr[j];
+            } 
+            if(sum==k){
+                maxLen = Math.max(maxLen, j-i+1);
+            }
+            while(sum>k){
+                sum -= arr[i];
+                i++;
+            }
+        }
+        return maxLen;
     }
 
     public static void main(String[] args) {
         int[] input1 = {1,1,1};
         int k1 = 2;
+        System.out.println("Input : "+Arrays.toString(input1)+" Output : "+LongestSubarrayWithGivenSumPositiveNumbers.longestSubarrayBruteforce(input1,k1));
         System.out.println("Input : "+Arrays.toString(input1)+" Output : "+LongestSubarrayWithGivenSumPositiveNumbers.longestSubarray(input1,k1));
         System.out.println("------------------------------------------------------");
         int[] input2 = {10, 5, 2, 7, 1, 9};
         int k2 = 7;
-        System.out.println("Input : "+Arrays.toString(input2)+" Output : "+LongestSubarrayWithGivenSumPositiveAndNegativeNumbers.longestSubarray(input2,k2));
+        System.out.println("Input : "+Arrays.toString(input2)+" Output : "+LongestSubarrayWithGivenSumPositiveNumbers.longestSubarrayBruteforce(input2,k2));
+        System.out.println("Input : "+Arrays.toString(input2)+" Output : "+LongestSubarrayWithGivenSumPositiveNumbers.longestSubarray(input2,k2));
         System.out.println("------------------------------------------------------");
         
     }
