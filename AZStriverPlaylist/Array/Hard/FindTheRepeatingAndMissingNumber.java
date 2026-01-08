dbf7d5b4e246603277a2c0c2e986205c728b59c2
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-01-08 09:03:19 +0530

    Find the repeating and missing number

---
+++ AZStriverPlaylist/Array/Hard/FindTheRepeatingAndMissingNumber.java
@@ -0,0 +1,70 @@
+package AZStriverPlaylist.Array.Hard;
+
+import java.util.Arrays;
+
+public class FindTheRepeatingAndMissingNumber {
+    /*
+    * Time conplexity : O(n)
+    */
+    public static int[] findMissingRepeatingNumbers(int[] nums) {
+        int xorOfAB=0;
+        for(int i=0;i<nums.length;i++){
+            xorOfAB ^= nums[i];
+            xorOfAB ^= (i+1);
+        }
+
+        // int bitPosition=0;
+        // while(((1<<bitPosition)&xorOfAB)==0){
+        //     bitPosition++;
+        // }
+
+        // Use int rightMostSetBit = xorOfAB & -xorOfAB; // shortcut 
+        int bitPosition = xorOfAB & -xorOfAB;
+
+
+        int numA=0;
+        for(int i=0;i<nums.length;i++){
+            if((nums[i]&(1<<bitPosition))!=0){
+                numA ^=nums[i];
+            }
+
+            if(((i+1)&(1<<bitPosition))!=0){
+                numA ^=(i+1);
+            }
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==numA){
+                return new int[]{numA,numA^xorOfAB};
+            }
+        }
+
+        return new int[]{numA^xorOfAB,numA};
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = { 3, 5, 4, 1, 1 };
+        int[] nums2 = {1, 2, 3, 6, 7, 5, 7 };
+        int[] nums3 = { 6, 5, 7, 1, 8, 6, 4, 3, 2};
+        System.out.println("---------------------------------------------------");
+        System.out.println("Input : " + Arrays.toString(nums1));
+        System.out.println("Output : " +Arrays.toString(FindTheRepeatingAndMissingNumber.findMissingRepeatingNumbers(nums1)));
+        System.out.println("Output : " +Arrays.toString(FindTheRepeatingAndMissingNumber.findMissingRepeatingNumbers(nums1)));
+        System.out.println("Output : " +Arrays.toString(FindTheRepeatingAndMissingNumber.findMissingRepeatingNumbers(nums1)));
+
+        System.out.println("---------------------------------------------------");
+        System.out.println("Input : " + Arrays.toString(nums2));
+        System.out.println("Output : " + Arrays.toString(FindTheRepeatingAndMissingNumber.findMissingRepeatingNumbers(nums2)));
+        System.out.println("Output : " + Arrays.toString(FindTheRepeatingAndMissingNumber.findMissingRepeatingNumbers(nums2)));
+        System.out.println("Output : " + Arrays.toString(FindTheRepeatingAndMissingNumber.findMissingRepeatingNumbers(nums2)));
+
+        System.out.println("---------------------------------------------------");
+        System.out.println("Input : " + Arrays.toString(nums3));
+        System.out.println("Output : " + Arrays.toString(FindTheRepeatingAndMissingNumber.findMissingRepeatingNumbers(nums3)));
+        System.out.println("Output : " + Arrays.toString(FindTheRepeatingAndMissingNumber.findMissingRepeatingNumbers(nums3)));
+        System.out.println("Output : " + Arrays.toString(FindTheRepeatingAndMissingNumber.findMissingRepeatingNumbers(nums3)));
+
+        System.out.println("---------------------------------------------------");
+    }
+}
