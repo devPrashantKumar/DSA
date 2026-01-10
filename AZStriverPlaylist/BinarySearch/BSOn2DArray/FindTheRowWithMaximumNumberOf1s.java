5f0abc3cc845da4a895a775c2bb8e444b46a6256
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-01-10 10:06:27 +0530

    brute force approac added

--- AZStriverPlaylist/BinarySearch/BSOn2DArray/FindTheRowWithMaximumNumberOf1s.java
+++ AZStriverPlaylist/BinarySearch/BSOn2DArray/FindTheRowWithMaximumNumberOf1s.java
@@ -3,6 +3,20 @@ package AZStriverPlaylist.BinarySearch.BSOn2DArray;
 public class FindTheRowWithMaximumNumberOf1s {
 
     /*
+    * Time Complexity : O(n^2)
+    */
+    public static int rowWithMax1sBruteForce(int[][] mat) {
+        for(int col=0;col<mat[0].length;col++){
+            for(int row=0;row<mat.length;row++){
+                if(mat[row][col]==1){
+                    return row;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /*
     * Time Complexity : O(n log n)
     */
     public static int rowWithMax1s(int[][] mat) {
@@ -33,6 +47,10 @@ public class FindTheRowWithMaximumNumberOf1s {
         int[][] nums1 = {{1, 1, 1}, {0, 0, 1}, {0, 0, 0}};
         int[][] nums2 = {{0,0}, {0, 0}};
         int[][] nums3 = {{0, 0, 1}, {0, 1, 1}, {0, 1, 1}};
+        System.out.println(FindTheRowWithMaximumNumberOf1s.rowWithMax1sBruteForce(nums2));
+        System.out.println(FindTheRowWithMaximumNumberOf1s.rowWithMax1sBruteForce(nums1)); 
+        System.out.println(FindTheRowWithMaximumNumberOf1s.rowWithMax1sBruteForce(nums3));
+        System.out.println("---------------------------------------------");
         System.out.println(FindTheRowWithMaximumNumberOf1s.rowWithMax1s(nums2));
         System.out.println(FindTheRowWithMaximumNumberOf1s.rowWithMax1s(nums1)); 
         System.out.println(FindTheRowWithMaximumNumberOf1s.rowWithMax1s(nums3));
