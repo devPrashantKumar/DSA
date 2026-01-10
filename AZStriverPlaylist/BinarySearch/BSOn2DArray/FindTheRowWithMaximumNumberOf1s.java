2f66b4a0f8e382ca2f031bfd337c3afba8976c1f
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-01-10 12:25:07 +0530

    FindTheRowWithMaximumNumberOf1s

--- AZStriverPlaylist/BinarySearch/BSOn2DArray/FindTheRowWithMaximumNumberOf1s.java
+++ AZStriverPlaylist/BinarySearch/BSOn2DArray/FindTheRowWithMaximumNumberOf1s.java
@@ -17,6 +17,26 @@ public class FindTheRowWithMaximumNumberOf1s {
     }
 
     /*
+    * Time Complexity : O(n^2)
+    */
+    public static int rowWithMax1sBruteForce2(int[][] mat) {
+        int index = -1;
+        int maxCount=0;
+
+        for(int row=0;row<mat.length;row++ ){
+            int count=0;
+            for(int col=0;col<mat[row].length;col++){
+                if(mat[row][col]==1) count++;
+            }
+            if(count>maxCount){
+                maxCount=count;
+                index = row;
+            } 
+        }
+        return index;
+    }
+
+    /*
     * Time Complexity : O(n log n)
     */
     public static int rowWithMax1s(int[][] mat) {
@@ -26,6 +46,8 @@ public class FindTheRowWithMaximumNumberOf1s {
         while(left<=right){
             boolean found=false;
             int mid = (left+(right-left)/2);
+
+
             for(int row=0;row<mat.length;row++){
                 if(mat[row][mid]==1){
                     rowIndex=row;
@@ -43,17 +65,60 @@ public class FindTheRowWithMaximumNumberOf1s {
         return rowIndex;
     }
 
+    /*
+    * Time Complexity : O(n log n)
+    */
+    public static int rowWithMax1s2(int[][] mat) {
+        int maxIndex = -1;
+        int maxCount=0;
+        
+        for(int row=0;row<mat.length;row++ ){
+            int left=0;
+            int right=mat[row].length-1;
+            int index = mat[row].length;
+
+            while(left<=right){
+                int mid = (left+(right-left)/2);
+                if(mat[row][mid]>=1){
+                    index = mid;
+                    right = mid-1;
+                }
+                else{
+                    left = mid+1;
+                }
+            }
+            
+            int count = mat[row].length-index;
+            if(count>maxCount){
+                maxCount=count;
+                maxIndex=row;
+            }
+        }
+
+        return maxIndex;
+    }
+
     public static void main(String[] args) {
         int[][] nums1 = {{1, 1, 1}, {0, 0, 1}, {0, 0, 0}};
         int[][] nums2 = {{0,0}, {0, 0}};
         int[][] nums3 = {{0, 0, 1}, {0, 1, 1}, {0, 1, 1}};
-        System.out.println(FindTheRowWithMaximumNumberOf1s.rowWithMax1sBruteForce(nums2));
-        System.out.println(FindTheRowWithMaximumNumberOf1s.rowWithMax1sBruteForce(nums1)); 
-        System.out.println(FindTheRowWithMaximumNumberOf1s.rowWithMax1sBruteForce(nums3));
+        System.out.println(FindTheRowWithMaximumNumberOf1s.rowWithMax1sBruteForce(nums1));
+        System.out.println(FindTheRowWithMaximumNumberOf1s.rowWithMax1sBruteForce2(nums1)); 
+        System.out.println(FindTheRowWithMaximumNumberOf1s.rowWithMax1s(nums1));
+        System.out.println(FindTheRowWithMaximumNumberOf1s.rowWithMax1s2(nums1));
+
         System.out.println("---------------------------------------------");
+        System.out.println(FindTheRowWithMaximumNumberOf1s.rowWithMax1sBruteForce(nums2));
+        System.out.println(FindTheRowWithMaximumNumberOf1s.rowWithMax1sBruteForce2(nums2)); 
         System.out.println(FindTheRowWithMaximumNumberOf1s.rowWithMax1s(nums2));
-        System.out.println(FindTheRowWithMaximumNumberOf1s.rowWithMax1s(nums1)); 
+        System.out.println(FindTheRowWithMaximumNumberOf1s.rowWithMax1s2(nums2));
+
+        System.out.println("---------------------------------------------");
+        System.out.println(FindTheRowWithMaximumNumberOf1s.rowWithMax1sBruteForce(nums3));
+        System.out.println(FindTheRowWithMaximumNumberOf1s.rowWithMax1sBruteForce2(nums3)); 
         System.out.println(FindTheRowWithMaximumNumberOf1s.rowWithMax1s(nums3));
+        System.out.println(FindTheRowWithMaximumNumberOf1s.rowWithMax1s2(nums3));
+
         System.out.println("---------------------------------------------");
     }
 }
