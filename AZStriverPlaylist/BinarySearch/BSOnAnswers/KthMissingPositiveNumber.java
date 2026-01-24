9df91644017acbef988b597f649c0ae4e8623cf3
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-01-24 18:14:10 +0530

    KthMissingPositiveNumber

---
+++ AZStriverPlaylist/BinarySearch/BSOnAnswers/KthMissingPositiveNumber.java
@@ -0,0 +1,37 @@
+package AZStriverPlaylist.BinarySearch.BSOnAnswers;
+
+public class KthMissingPositiveNumber {
+    /*
+    * Time Complexity : O(n)
+    */
+    public static int findKthPositive(int[] arr, int k) {
+        int kthMissing = k;
+        for(int num:arr){
+            if(num<=kthMissing) kthMissing++;
+            else return kthMissing;
+        }
+        return kthMissing;
+    }
+
+    /*
+    * Time Complexity : O(log n)
+    */
+    public static int findKthPositiveUsingBS(int[] arr, int k) {
+        int left=0;
+        int right = arr.length-1;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            if(arr[mid]>k+mid) right = mid-1;
+            else left = mid+1;
+        }
+        return left+k;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findKthPositive(new int[] { 2,3,4,7,11 }, 5));
+        System.out.println(findKthPositive(new int[] { 1,2,3,4 }, 2));
+        System.out.println("===================================");
+        System.out.println(findKthPositiveUsingBS(new int[] { 2,3,4,7,11 }, 5));
+        System.out.println(findKthPositiveUsingBS(new int[] { 1,2,3,4 }, 2));
+    }
+}
