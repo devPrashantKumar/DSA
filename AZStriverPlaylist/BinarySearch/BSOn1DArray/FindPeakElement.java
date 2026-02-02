44e2f7c644fe71e7a3a30844290d884f0cc2e24a
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-02-02 21:42:41 +0530

    FindPeakElement

--- AZStriverPlaylist/BinarySearch/BSOn1DArray/FindPeakElement.java
+++ AZStriverPlaylist/BinarySearch/BSOn1DArray/FindPeakElement.java
@@ -1,6 +1,24 @@
 package AZStriverPlaylist.BinarySearch.BSOn1DArray;
 
 public class FindPeakElement {
+
+    /*
+    * Time Complexity : O(n)
+    */
+    public static int findPeakElementBruteForce(int[] nums) {
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            int prev = (i-1<0) ? Integer.MIN_VALUE : nums[i-1];
+            int next = (i+1>=n) ? Integer.MIN_VALUE : nums[i+1];
+            if(prev < nums[i] && nums[i] > next) return i;
+        }
+        return -1;
+    }
+
+
+    /*
+    * Time Complexity : O(n log n) using recursion
+    */
     public static int findPeakElement(int[] nums) {
         int n =  nums.length;
         int left = 0;
@@ -40,10 +58,37 @@ public class FindPeakElement {
         if(rightHalf[0]==1) return rightHalf;
         return new int[]{0,0};
     }
+
+    /*
+    * Time Complexity : O(log n)
+    */
+    public static int findPeakElementUsingBinarySearch(int[] nums) {
+        int n = nums.length;
+        if(n==0) return 0;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[n-1]>nums[n-2]) return n-1;
+        int left = 1;
+        int right = n-2;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]) return mid;
+            else if(nums[mid-1]<nums[mid]) left = mid+1;
+            else if(nums[mid]>nums[mid+1]) right = mid-1;
+            else left = mid-1;
+        }
+        return -1;
+    }
         
 
     public static void main(String[] args) {
+        System.out.println(findPeakElementBruteForce(new int[]{1,2,3,1}));
         System.out.println(findPeakElement(new int[]{1,2,3,1}));
+        System.out.println(findPeakElementUsingBinarySearch(new int[]{1,2,3,1}));
+
+        System.out.println("-----------------------------------------------");
+        System.out.println(findPeakElementBruteForce(new int[]{1,2,1,3,5,6,4}));
         System.out.println(findPeakElement(new int[]{1,2,1,3,5,6,4}));
+        System.out.println(findPeakElementUsingBinarySearch(new int[]{1,2,1,3,5,6,4}));
+
     }
 }
