3357acc472b113d90506acecdd1237a7857aeb28
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-02-06 14:58:59 +0530

    RotateString

--- AZStriverPlaylist/Strings/RotateString.java
+++ AZStriverPlaylist/Strings/RotateString.java
@@ -1,6 +1,10 @@
 package AZStriverPlaylist.Strings;
 
 public class RotateString {
+    /*
+    * Time:  O(n²)
+    * Space: O(n)
+    */
     public static boolean rotateStringSimpleApproach(String s, String goal) {
         if(s.length()!=goal.length()) return false;
         String sTemp = s+s;
@@ -16,16 +20,27 @@ public class RotateString {
         return false;
     }
 
+    /*
+    * Time:  O(n) - optimized substring search internally
+    * Space: O(n)
+    */
+    public static boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) return false;
+        return (s + s).contains(goal);
+    }
+
     public static void main(String[] args) {
         String s1 = "abcde";
         String goal1 = "cdeab";
         System.out.println("Input : s - "+s1+" , goal - "+goal1);
+        System.out.println("Output : " + RotateString.rotateString(s1,goal1));
         System.out.println("Output : " + RotateString.rotateStringSimpleApproach(s1,goal1));
         System.out.println("---------------------------------------------------");
         
         String s2 = "abcde";
         String goal2 = "abced";
         System.out.println("Input : s - "+s2+" , goal - "+goal2);
+        System.out.println("Output : " + RotateString.rotateString(s2,goal2));
         System.out.println("Output : " + RotateString.rotateStringSimpleApproach(s2,goal2));
         System.out.println("---------------------------------------------------");
         
