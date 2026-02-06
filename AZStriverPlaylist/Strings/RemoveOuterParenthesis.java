2efa95849be74bdd8822bea42186eeda22e10bfd
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-02-06 07:43:37 +0530

    RemoveOuterParenthesis

--- AZStriverPlaylist/Strings/RemoveOuterParenthesis.java
+++ AZStriverPlaylist/Strings/RemoveOuterParenthesis.java
@@ -23,12 +23,68 @@ public class RemoveOuterParenthesis {
         return str.toString();
     }
 
+    /*
+     * TIme Complexity : O(n)
+     * Space Complexity : O(1)
+     */
+    public static String removeOuterParenthesesOptimised(String s) {
+        StringBuilder str = new StringBuilder();
+        int counter=0;
+        for(int k=0;k<s.length();k++){
+            if(counter==0){
+                counter++;
+                continue;
+            } 
+            if(s.charAt(k)=='(') counter++;
+            else counter--;
+
+            if(counter!=0){
+                str.append(s.charAt(k));
+            }
+        }
+        return str.toString();
+    }
+
+    /*
+     * TIme Complexity : O(n)
+     * Space Complexity : O(1)
+     */
+    public static String removeOuterParenthesesOptimised2(String s) {
+        StringBuilder str = new StringBuilder();
+        int counter=0;
+        for(char k : s.toCharArray()){
+            if(k=='('){
+                if(counter>0) str.append(k);
+                counter++;
+            }else{
+                counter--;
+                if(counter>0) str.append(k);
+            }
+        }
+        return str.toString();
+    }
+
+
     public static void main(String[] args) {
         String input1 = "(()())(())";
         System.out.println("Input : "+input1+" Output : "+RemoveOuterParenthesis.removeOuterParentheses(input1) );
+        System.out.println("Input : "+input1+" Output : "+RemoveOuterParenthesis.removeOuterParenthesesOptimised(input1) );
+        System.out.println("Input : "+input1+" Output : "+RemoveOuterParenthesis.removeOuterParenthesesOptimised2(input1) );
+
+        System.out.println("--------------------------------------------");
         String input2 = "(()())(())(()(()))";
         System.out.println("Input : "+input2+" Output : "+RemoveOuterParenthesis.removeOuterParentheses(input2) );
+        System.out.println("Input : "+input2+" Output : "+RemoveOuterParenthesis.removeOuterParenthesesOptimised(input2) );
+        System.out.println("Input : "+input2+" Output : "+RemoveOuterParenthesis.removeOuterParenthesesOptimised2(input2) );
+
+        System.out.println("--------------------------------------------");
+
         String input3 = "()()";
         System.out.println("Input : "+input3+" Output : "+RemoveOuterParenthesis.removeOuterParentheses(input3) );
+        System.out.println("Input : "+input3+" Output : "+RemoveOuterParenthesis.removeOuterParenthesesOptimised(input3) );
+        System.out.println("Input : "+input3+" Output : "+RemoveOuterParenthesis.removeOuterParenthesesOptimised2(input3) );
+
+        System.out.println("--------------------------------------------");
+
     }
 }
