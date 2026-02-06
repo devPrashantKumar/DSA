cbabd66a5dc7ae380486dffac07f2e72bd4a0a8b
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-02-06 08:21:00 +0530

    ReverseWordsInGivenString

--- AZStriverPlaylist/Strings/ReverseWordsInGivenString.java
+++ AZStriverPlaylist/Strings/ReverseWordsInGivenString.java
@@ -30,12 +30,40 @@ public class ReverseWordsInGivenString {
         return result.toString();
     }
 
+    public static String reverseWordsApproach2(String s) {
+        StringBuilder word = new StringBuilder();
+        StringBuilder result =  new StringBuilder();
+        for (int i = s.length()-1; i >=0 ; i--) {
+            if (s.charAt(i) != ' ') {
+                word.append(s.charAt(i));
+            } else {
+                if (word.length() >0 ) {
+                    result.append(word.reverse().toString());
+                    result.append(" ");
+                    word.setLength(0);
+                }
+            }
+        }
+
+        if (word.length() >0 ) {
+            result.append(word.reverse().toString());
+        }
+
+        return result.toString();
+    }
+
     public static void main(String[] args) {
         String input1 = "the sky is blue";
         System.out.println("Input : "+input1+" Output : "+ReverseWordsInGivenString.reverseWords(input1) );
+        System.out.println("Input : "+input1+" Output : "+ReverseWordsInGivenString.reverseWordsApproach2(input1) );
+        System.out.println("----------------------------------------");
         String input2 = "  hello world  ";
         System.out.println("Input : "+input2+" Output : "+ReverseWordsInGivenString.reverseWords(input2) );
+        System.out.println("Input : "+input2+" Output : "+ReverseWordsInGivenString.reverseWordsApproach2(input2) );
+        System.out.println("----------------------------------------");
         String input3 = "a good   example";
         System.out.println("Input : "+input3+" Output : "+ReverseWordsInGivenString.reverseWords(input3) );
+        System.out.println("Input : "+input3+" Output : "+ReverseWordsInGivenString.reverseWordsApproach2(input3) );
+
     }
 }
