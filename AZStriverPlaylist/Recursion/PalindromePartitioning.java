7f7be009670959d486a8dd72f7963f519b9bfbfe
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-02-15 11:41:10 +0530

    PalindromePartitioning

--- AZStriverPlaylist/Recursion/PalindromePartitioning.java
+++ AZStriverPlaylist/Recursion/PalindromePartitioning.java
@@ -29,6 +29,38 @@ public class PalindromePartitioning {
         }
     }
 
+    public static List<List<String>> partition2(String str){
+        List<List<String>> result = new ArrayList<>();
+        partitionUtil2(str, 0,true, new ArrayList<>(), result);
+        return result;
+    }
+
+    /*
+     * using StringBuilder instead of String
+     */
+    public static void partitionUtil2(String str, int index, boolean prevPalindrom, List<StringBuilder> partitionList, List<List<String>> result){
+        if(index==str.length()){
+            if(prevPalindrom) result.add(new ArrayList<>(partitionList.stream().map(StringBuilder::toString).toList()));
+            return;
+        }
+
+        if(prevPalindrom){
+            StringBuilder strTemp = new StringBuilder();
+            strTemp.append(str.charAt(index));
+            partitionList.add(strTemp);
+            partitionUtil2(str, index+1, true, partitionList, result);
+            partitionList.remove(partitionList.size()-1);
+        }
+
+        if(!partitionList.isEmpty()){
+            StringBuilder strTemp2 = partitionList.get(partitionList.size()-1);
+            strTemp2.append(str.charAt(index));
+            prevPalindrom = checkPalindrom(strTemp2, 0, strTemp2.length()-1);
+            partitionUtil2(str, index+1, prevPalindrom, partitionList, result);
+            strTemp2.deleteCharAt(strTemp2.length()-1);
+        }
+    }
+
     public static boolean checkPalindrom(StringBuilder str, int left, int right){
         if(left>=right) return true;
         if(str.charAt(left)!=str.charAt(right)) return false;
@@ -41,5 +73,14 @@ public class PalindromePartitioning {
 
         String str2="a";
         System.out.println("Input 1 " + str2+" Output : "+PalindromePartitioning.partition(str2));
+        System.out.println("------------------------------------------------------------------------------");
+        String str3="aab";
+        System.out.println("Input 1 " + str3+" Output : "+PalindromePartitioning.partition2(str3));
+
+        String str4="a";
+        System.out.println("Input 1 " + str4+" Output : "+PalindromePartitioning.partition2(str4));
+
+        String str5="aabaa";
+        System.out.println("Input 1 " + str5+" Output : "+PalindromePartitioning.partition2(str5));
     }
 }
