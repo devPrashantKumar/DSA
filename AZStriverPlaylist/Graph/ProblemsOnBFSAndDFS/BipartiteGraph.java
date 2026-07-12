6994be46312b63ab87fabdefe9b347d58440390f
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-07-12 15:25:01 +0530

    Add isBipartiteGraphDFS2 method and utility for bipartite checking using DFS

--- AZStriverPlaylist/Graph/ProblemsOnBFSAndDFS/BipartiteGraph.java
+++ AZStriverPlaylist/Graph/ProblemsOnBFSAndDFS/BipartiteGraph.java
@@ -35,6 +35,28 @@ public class BipartiteGraph {
         return true;
     }
 
+    public static boolean isBipartiteGraphDFS2(int V, List<List<Integer>> edges) {
+        int[] visited = new int[V];
+        for(int i=0;i<visited.length;i++) visited[i]=-1;
+        for(int i=0;i<V;i++){
+            if(visited[i]==-1){
+                if(!isBipartiteGraphDFSUtil2(edges,visited,i,0)) return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isBipartiteGraphDFSUtil2(List<List<Integer>> edges, int[] visited, int vertex, int color) {
+        visited[vertex]=color;
+        for(int j=0;j<edges.get(vertex).size();j++){
+            if(visited[edges.get(vertex).get(j)]==-1){
+                if(!isBipartiteGraphDFSUtil2(edges,visited,edges.get(vertex).get(j),1-color))return false;
+            }
+            else if(visited[edges.get(vertex).get(j)]==color) return false;
+        } 
+        return true;
+    }
+
 
     public static boolean isBipartiteGraphBFS(int V, List<List<Integer>> edges) {
         int[] visited = new int[V];
@@ -91,6 +113,7 @@ public class BipartiteGraph {
         int V1 = 4;
         List<List<Integer>> edges1 = List.of(List.of(1),List.of(2,0,3), List.of(1,3),List.of(1,2));
         System.out.println(isBipartiteGraphDFS(V1,edges1));
+        System.out.println(isBipartiteGraphDFS2(V1,edges1));
         System.out.println(isBipartiteGraphBFS(V1,edges1));
 
         System.out.println("----------------------------------------------------------------------");
@@ -98,6 +121,7 @@ public class BipartiteGraph {
         int V2 = 7;
         List<List<Integer>> edges2 = List.of(List.of(1),List.of(0,2), List.of(1,3),List.of(2),List.of(5),List.of(4),List.of());
         System.out.println(isBipartiteGraphDFS(V2,edges2));
+        System.out.println(isBipartiteGraphDFS2(V2,edges2));
         System.out.println(isBipartiteGraphBFS(V2,edges2));
 
         System.out.println("----------------------------------------------------------------------");
