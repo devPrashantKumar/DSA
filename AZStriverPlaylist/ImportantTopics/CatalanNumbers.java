94109bb81192fde95798a1630d1cd3a9e5de8f72
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-07-20 00:33:46 +0530

    Add CatalanNumbers class with implementation for calculating Catalan numbers

---
+++ AZStriverPlaylist/ImportantTopics/CatalanNumbers.java
@@ -0,0 +1,24 @@
+package AZStriverPlaylist.ImportantTopics;
+
+public class CatalanNumbers {
+
+    public static long catalan(int n) {
+        long[] dp = new long[n + 1];
+
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = 0;
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - 1 - j];
+            }
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(catalan(n)); // 42
+    }
+}
