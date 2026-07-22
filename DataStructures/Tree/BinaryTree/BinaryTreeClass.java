4a4e61b02a09243e9faa69179a22eecd7bf64d83
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-07-22 14:47:34 +0530

    code refactoring

--- DataStructures/Tree/BinaryTree/BinaryTreeClass.java
+++ DataStructures/Tree/BinaryTree/BinaryTreeClass.java
@@ -3,7 +3,7 @@ package DataStructures.Tree.BinaryTree;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class BinaryTree {
+public class BinaryTreeClass {
     class BinaryTreeNode {
         int data;
         BinaryTreeNode left;
@@ -228,6 +228,59 @@ public class BinaryTree {
                 prev = current;
             }
         }
+    }
+
+    public static void main(String[] args) {
+        BinaryTreeClass btc = new BinaryTreeClass();
+        try {
+            btc.printBinaryTree();
+        } catch (Exception e) {
+           System.out.println(e);
+        }
+
+        btc.preOrderTraversal();
+        System.out.println();
+
+        btc.addRoot(11);
+        btc.addLeftChild(btc.root, 12);
+        btc.addRightChild(btc.root, 13);
+        btc.addLeftChild(btc.root.left, 14);
+        btc.addRightChild(btc.root.left, 15);
+        btc.addLeftChild(btc.root.right, 16);
+        btc.addRightChild(btc.root.right, 17);
+        btc.addLeftChild(btc.root.left.left, 18);
+        btc.addRightChild(btc.root.left.left, 19);
+
+        btc.preOrderTraversal();
+        System.out.println();
+        btc.inOrderTraversal();
+        System.out.println();
+        btc.postOrderTraversal();
+        System.out.println();
+        btc.levelOrderTraversal();
+        System.out.println();
+
+        System.out.println("15 is available "+btc.searchUsingRecursion(btc.root, 15));
+        System.out.println("20 is available "+btc.searchUsingRecursion(btc.root, 20));
+        System.out.println("15 is available "+btc.searchUsinglevelOrderTraversal(15));
+        System.out.println("20 is available "+btc.searchUsinglevelOrderTraversal(20));
+
+        BinaryTreeClass btc2 = new BinaryTreeClass();
+        btc2.insertBinaryTreeNode(11);
+        btc2.insertBinaryTreeNode(12);
+        btc2.insertBinaryTreeNode(13);
+        btc2.insertBinaryTreeNode(14);
+        btc2.insertBinaryTreeNode(15);
+        btc2.insertBinaryTreeNode(16);
+        btc2.insertBinaryTreeNode(17);
+        btc2.insertBinaryTreeNode(18);
+        btc2.insertBinaryTreeNode(19);
+        btc2.levelOrderTraversal();
+        System.out.println();
+        btc2.deleteBinaryTreeNode(13);
+        btc2.levelOrderTraversal();
+        System.out.println();
+        btc2.printBinaryTree();
     }
 
 }
---
+++ DataStructures/Tree/BinaryTree/BinaryTreeNodeClass.java
@@ -0,0 +1,44 @@
+package DataStructures.Tree.BinaryTree;
+
+public class BinaryTreeNodeClass {
+    int data;
+    BinaryTreeNodeClass left;
+    BinaryTreeNodeClass right;
+
+    public BinaryTreeNodeClass(int data){
+        this.data = data;
+    }
+
+    public void addLeftChild(BinaryTreeNodeClass child){
+        this.left = child;
+    }
+
+    public void addRightChild(BinaryTreeNodeClass child){
+        this.right = child;
+    }
+
+    public void printBinaryTree(int level){
+        String indentation = " ".repeat(level);
+        System.out.println(indentation+data);
+        if(left!=null)left.printBinaryTree(level+1);
+        if(right!=null)right.printBinaryTree(level+1);
+    }
+
+    public static void main(String[] args) {
+        BinaryTreeNodeClass node1 = new BinaryTreeNodeClass(10);
+        BinaryTreeNodeClass node2 = new BinaryTreeNodeClass(11);
+        BinaryTreeNodeClass node3 = new BinaryTreeNodeClass(12);
+        BinaryTreeNodeClass node4 = new BinaryTreeNodeClass(13);
+        BinaryTreeNodeClass node5 = new BinaryTreeNodeClass(14);
+        BinaryTreeNodeClass node6 = new BinaryTreeNodeClass(15);
+    
+        BinaryTreeNodeClass root = node1;
+        node1.addLeftChild(node2);
+        node1.addRightChild(node3);
+        node2.addLeftChild(node4);
+        node3.addLeftChild(node5);
+        node3.addRightChild(node6);
+
+        root.printBinaryTree(0);
+    }
+}
