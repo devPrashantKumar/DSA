d4fd029a1e3bc234c91a6e50d5d856bb078d3f06
Prashant Kumar <77582926+devPrashantKumar@users.noreply.github.com>
2026-07-27 07:11:23 +0530

    Update FindTheNthRootOfANumberUsingBinarySearch.java

--- AZStriverPlaylist/BinarySearch/BSOnAnswers/FindTheNthRootOfANumberUsingBinarySearch.java
+++ AZStriverPlaylist/BinarySearch/BSOnAnswers/FindTheNthRootOfANumberUsingBinarySearch.java
@@ -14,7 +14,7 @@ public class FindTheNthRootOfANumberUsingBinarySearch {
         while (left <= right) {
             int mid = left + (right - left) / 2;
             double power = Math.pow(mid, N);
-            //if (power == (double) M)
+            //if (power == (double) M) - this line will not work due to double precision
             //if ((int)power == M)
             if (Math.abs(power - M) < 1e-9)
                 return mid;
