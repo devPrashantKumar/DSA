781a4e21eaea1a57c29720ab2a21cb882c87a6cd
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-08-03 05:27:16 +0530

    feat: implement second method for shortest common supersequence using dynamic programming

--- AZStriverPlaylist/DynamicProgramming/DPOnStrings/ShortestCommonSupersequence.java
+++ AZStriverPlaylist/DynamicProgramming/DPOnStrings/ShortestCommonSupersequence.java
@@ -19,18 +19,61 @@ public class ShortestCommonSupersequence {
         return strb.toString();
     }
 
+    public static String shortestCommonSupersequence2(String str1, String str2) {
+        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
+        StringBuilder strb = new StringBuilder();
+        for (int i = 1; i <= str1.length(); i++) {
+            for (int j = 1; j <= str2.length(); j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1))
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+            }
+        }
+        int i = str1.length();
+        int j = str2.length();
+        while (i > 0 && j > 0) {
+            if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                strb.append(str1.charAt(i - 1));
+                i--;
+                j--;
+            } else {
+                if (dp[i - 1][j] >= dp[i][j - 1]){
+                    strb.append(str1.charAt(i-1));
+                    i--;
+                }
+                else{
+                    strb.append(str2.charAt(j-1));
+                    j--;
+                }
+            }
+
+        }
+        while (i > 0) {
+            strb.append(str1.charAt(i-1));
+            i--;    
+        }
+        while (j > 0) {            
+            strb.append(str2.charAt(j-1));
+            j--;
+        }
+        return strb.reverse().toString();
+    }
+
 
     public static void main(String[] args) {
         String str1 = "mno";
         String str2 = "nop";
 
         System.out.println(shortestCommonSupersequence(str1,str2));
+        System.out.println(shortestCommonSupersequence2(str1,str2));
 
         System.out.println("--------------------------------------------------");
         String str11 = "dynamic";
         String str12 = "program";
 
         System.out.println(shortestCommonSupersequence(str11,str12));
+        System.out.println(shortestCommonSupersequence2(str11,str12));
 
         System.out.println("--------------------------------------------------");
     }
