05d470c20d6697bafeab3c6eb18f705f47b529e6
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-08-06 06:51:07 +0530

    feat: add frogJump2 method and its utility for frog jump problem; implement tabulation2 method for optimized jumping

--- AZStriverPlaylist/DynamicProgramming/OneDimensionDP/FrogJumpWithKDistances.java
+++ AZStriverPlaylist/DynamicProgramming/OneDimensionDP/FrogJumpWithKDistances.java
@@ -21,6 +21,25 @@ public class FrogJumpWithKDistances {
     }
 
     /*
+     * Time Complexity : O(k^n)
+     * Space Complexity : O(n), (recursive stack)
+     */
+    public static int frogJump2(int[] heights, int k) {
+        return frogJumpUtil2(heights, k, 0);
+    }
+
+    public static int frogJumpUtil2(int[] heights, int k, int start) {
+        if (start == heights.length-1)
+            return 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; (i <= k && i+start < heights.length); i++) {
+            int step = Math.abs(heights[start] - heights[start+i]) + frogJumpUtil2(heights, k, start+i);
+            min = Math.min(step, min);
+        }
+        return min;
+    }
+
+    /*
      * Time Complexity : O(k*n)
      * Space Complexity : O(n)+O(n) , (dp+recursive stack)
      */
@@ -45,12 +64,12 @@ public class FrogJumpWithKDistances {
     }
 
     /*
+     * Standard approach
      * Time Complexity : O(k*n)
-     * Space Complexity : O(n)
+     * Space Complexity : O(n)+
      */
     public static int frogJumpTabulation(int[] heights, int k) {
-        Integer[] dp = new Integer[heights.length];
-        dp[0] = 0;
+        int[] dp = new int[heights.length];
         for (int i = 1; i < heights.length; i++) {
             int min = Integer.MAX_VALUE;
             for (int j = 1; (j <= k && i - j >= 0); j++) {
@@ -63,6 +82,23 @@ public class FrogJumpWithKDistances {
     }
 
     /*
+     * Time Complexity : O(k*n)
+     * Space Complexity : O(n)
+     */
+    public static int frogJumpTabulation2(int[] heights, int k) {
+        int[] dp = new int[heights.length];
+        for (int i = heights.length-2; i >=0 ; i--) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 1; (j <= k && i + j < heights.length); j++) {
+                int step = Math.abs(heights[i] - heights[i + j]) + dp[i + j];
+                min = Math.min(step, min);
+            }
+            dp[i] = min;
+        }
+        return dp[0];
+    }
+
+    /*
      * IMPORTANT
      * Time Complexity : O(k*n)
      * Space Complexity : O(k)
@@ -70,7 +106,6 @@ public class FrogJumpWithKDistances {
     public static int frogJumpSpaceOptimized(int[] heights, int k) {
         int n = heights.length;
         int[] dp = new int[k];
-        dp[0] = 0;
         for (int i = 1; i < n; i++) {
             int min = Integer.MAX_VALUE;
             for (int j = 1; j <= k && i - j >= 0; j++) {
@@ -84,13 +119,17 @@ public class FrogJumpWithKDistances {
 
     public static void main(String[] args) {
         System.out.println(frogJump(new int[] { 10, 5, 20, 0, 15 }, 2));
+        System.out.println(frogJump2(new int[] { 10, 5, 20, 0, 15 }, 2));
         System.out.println(frogJumpMemoization(new int[] { 10, 5, 20, 0, 15 }, 2));
         System.out.println(frogJumpTabulation(new int[] { 10, 5, 20, 0, 15 }, 2));
+        System.out.println(frogJumpTabulation2(new int[] { 10, 5, 20, 0, 15 }, 2));
         System.out.println(frogJumpSpaceOptimized(new int[] { 10, 5, 20, 0, 15 }, 2));
 
         System.out.println("----------------------------------------------------");
 
         System.out.println(frogJump(new int[] { 15, 4, 1, 14, 15 }, 3));
+                System.out.println(frogJump(new int[] { 15, 4, 1, 14, 15 }, 3));
+
         System.out.println(frogJumpMemoization(new int[] { 15, 4, 1, 14, 15 }, 3));
         System.out.println(frogJumpTabulation(new int[] { 15, 4, 1, 14, 15 }, 3));
         System.out.println(frogJumpSpaceOptimized(new int[] { 15, 4, 1, 14, 15 }, 3));
