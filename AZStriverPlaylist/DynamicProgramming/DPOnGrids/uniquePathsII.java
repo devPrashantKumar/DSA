3ee80945939ca1e18b14daccc787cef7cc81cec1
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-08-07 14:28:44 +0530

    fix: correct return statements in uniquePathsMemoization and uniquePathsTabulation methods; update initialization logic for dp arrays

--- AZStriverPlaylist/DynamicProgramming/DPOnGrids/uniquePathsII.java
+++ AZStriverPlaylist/DynamicProgramming/DPOnGrids/uniquePathsII.java
@@ -30,8 +30,7 @@ public class uniquePathsII {
         for (int[] row : dp) {
             Arrays.fill(row, -1);
         }
-        uniquePathsMemoizationUtil(0, 0, obstacleGrid, dp);
-        return dp[0][0];
+        return uniquePathsMemoizationUtil(0, 0, obstacleGrid, dp);
     }
 
     public static int uniquePathsMemoizationUtil(int row, int col, int[][] obstacleGrid, int[][] dp) {
@@ -53,22 +52,19 @@ public class uniquePathsII {
      */
     public static int uniquePathsTabulation(int[][] obstacleGrid) {
         int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
-        dp[0][0]=1;
+        dp[0][0]= (obstacleGrid[0][0]==1) ? 0 : 1;
 
         for (int i = 1; i < obstacleGrid.length; i++){
             dp[i][0] = (dp[i-1][0]==0 || obstacleGrid[i][0]==1) ? 0 : 1;
         }
-            
         for (int i = 1; i < obstacleGrid[0].length; i++){
             dp[0][i] = (dp[0][i-1]==0 || obstacleGrid[0][i]==1) ? 0 : 1;
         }
-
         for (int i = 1; i < obstacleGrid.length; i++) {
             for (int j = 1; j < obstacleGrid[0].length; j++) {
                 dp[i][j] = (obstacleGrid[i][j]==1) ? 0 : dp[i - 1][j] + dp[i][j - 1];
             }
         }
-
         return dp[obstacleGrid.length - 1][obstacleGrid[0].length - 1];
     }
 
@@ -78,16 +74,15 @@ public class uniquePathsII {
      */
     public static int uniquePathsTabulationSpaceOptimised(int[][] obstacleGrid) {
         int[] dp = new int[obstacleGrid[0].length];
-        dp[0]=1;
+        dp[0]= (obstacleGrid[0][0]==1) ? 0 : 1;
+
         for (int i = 1; i < obstacleGrid[0].length; i++){
             dp[i] = (dp[i-1]==0 || obstacleGrid[0][i]==1) ? 0 : 1;
         }
-
         for (int i = 1; i < obstacleGrid.length; i++) {
             dp[0] = (dp[0]==0 || obstacleGrid[i][0]==1) ? 0 : 1;
             for (int j = 1; j < obstacleGrid[0].length; j++) {
                 dp[j] = (obstacleGrid[i][j]==1) ? 0 : dp[j] + dp[j - 1];
-
             }
         }
         return dp[obstacleGrid[0].length - 1];
