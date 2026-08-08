0a458f46bc0a5020ef286ee1bb1129636cb659da
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-08-09 01:15:05 +0530

    feat: add additional partition methods with early exit for odd total sum

--- AZStriverPlaylist/DynamicProgramming/DPOnSubsequences/PartitionEqualSubsetSum.java
+++ AZStriverPlaylist/DynamicProgramming/DPOnSubsequences/PartitionEqualSubsetSum.java
@@ -1,7 +1,13 @@
 package AZStriverPlaylist.DynamicProgramming.DPOnSubsequences;
 
+import java.util.Arrays;
+
 public class PartitionEqualSubsetSum {
 
+    /*
+    * Time Complexity : O(2^n)
+    * Space Complexity : O(n) , recursion stack space
+    */
     public static boolean canPartition(int[] nums) {
         return canPartitionUtil(nums,0, 0, 0);
     }
@@ -15,13 +21,47 @@ public class PartitionEqualSubsetSum {
         return chooseSubset1 || chooseSubset2;
     }
 
+    public static boolean canPartition2(int[] nums) {
+        int totalSum = Arrays.stream(nums).sum();
+        if(totalSum%2!=0) return false;
+        return SubsetSumEqualToTarget.isSubsetSum(nums, totalSum/2);
+    }
+
+    public static boolean canPartitionMemoization(int[] nums) {
+        int totalSum = Arrays.stream(nums).sum();
+        if(totalSum%2!=0) return false;
+        return SubsetSumEqualToTarget.isSubsetSumMemoization(nums, totalSum/2);
+    }
+
+    public static boolean canPartitionTabulation(int[] nums) {
+        int totalSum = Arrays.stream(nums).sum();
+        if(totalSum%2!=0) return false;
+        return SubsetSumEqualToTarget.isSubsetSumTabulation(nums, totalSum/2);
+    }
+
+    public static boolean canPartitionTabulationSpaceOptimized(int[] nums) {
+        int totalSum = Arrays.stream(nums).sum();
+        if(totalSum%2!=0) return false;
+        return SubsetSumEqualToTarget.isSubsetSumTabulationSpaceOptimised(nums, totalSum/2);
+    }
+
     public static void main(String[] args) {
         int[] nums = {1,5,11,5};
         System.out.println(canPartition(nums));
+        System.out.println(canPartition2(nums));
+        System.out.println(canPartitionMemoization(nums));
+        System.out.println(canPartitionTabulation(nums));
+        System.out.println(canPartitionTabulationSpaceOptimized(nums));
 
         System.out.println("--------------------------------------------");
 
         int[] nums1 = {1,2,3,5};
         System.out.println(canPartition(nums1));
+        System.out.println(canPartition2(nums1));
+        System.out.println(canPartitionMemoization(nums1));
+        System.out.println(canPartitionTabulation(nums1));
+        System.out.println(canPartitionTabulationSpaceOptimized(nums1));
+
+
     }
 }
