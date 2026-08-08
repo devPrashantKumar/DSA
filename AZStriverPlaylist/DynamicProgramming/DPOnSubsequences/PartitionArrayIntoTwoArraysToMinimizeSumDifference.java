dcec60487ba2024d1c7e7d57e4c64a971d4968bd
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-08-08 19:21:17 +0530

    feat: implement Partition Array Into Two Arrays To Minimize Sum Difference algorithm

---
+++ AZStriverPlaylist/DynamicProgramming/DPOnSubsequences/PartitionArrayIntoTwoArraysToMinimizeSumDifference.java
@@ -0,0 +1,32 @@
+package AZStriverPlaylist.DynamicProgramming.DPOnSubsequences;
+
+public class PartitionArrayIntoTwoArraysToMinimizeSumDifference {
+    public static int minimumDifference(int[] nums) {
+        return minimumDifferenceUtil(nums,0, 0, 0, 0, 0);
+    }
+
+    public static int minimumDifferenceUtil(int[] nums, int index, int subset1, int subset2, int subset1Size, int subset2Size) {
+        if(index==nums.length){
+            if(subset1Size==subset2Size) return Math.abs(subset1-subset2);
+            else return Integer.MAX_VALUE;
+        }
+        int chooseSubset1 = minimumDifferenceUtil(nums,index+1, subset1+nums[index],subset2, subset1Size+1, subset2Size);
+        int chooseSubset2 = minimumDifferenceUtil(nums,index+1, subset1, subset2+nums[index], subset1Size, subset2Size+1);
+        return Math.min(chooseSubset1,chooseSubset2);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,9,7,3};
+        System.out.println(minimumDifference(nums));
+
+        System.out.println("--------------------------------------------");
+
+        int[] nums1 = {-36,36};
+        System.out.println(minimumDifference(nums1));
+
+        System.out.println("--------------------------------------------");
+
+        int[] nums2 = {2,-1,0,4,-2,-9};
+        System.out.println(minimumDifference(nums2));
+    }
+}
