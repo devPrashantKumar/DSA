390dcb7687292d55e217115314d18893ebb1c8aa
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-08-09 09:29:45 +0530

    feat: add alternative methods for minimum difference and partitioning problems

--- AZStriverPlaylist/DynamicProgramming/DPOnSubsequences/PartitionArrayIntoTwoArraysToMinimizeSumDifference.java
+++ AZStriverPlaylist/DynamicProgramming/DPOnSubsequences/PartitionArrayIntoTwoArraysToMinimizeSumDifference.java
@@ -1,5 +1,7 @@
 package AZStriverPlaylist.DynamicProgramming.DPOnSubsequences;
 
+import java.util.Arrays;
+
 public class PartitionArrayIntoTwoArraysToMinimizeSumDifference {
     public static int minimumDifference(int[] nums) {
         return minimumDifferenceUtil(nums,0, 0, 0, 0, 0);
@@ -15,18 +17,37 @@ public class PartitionArrayIntoTwoArraysToMinimizeSumDifference {
         return Math.min(chooseSubset1,chooseSubset2);
     }
 
+    public static int minimumDifference2(int[] nums) {
+        int totalSum = Arrays.stream(nums).sum();
+        return minimumDifferenceUtil2(nums,0, totalSum, 0, 0);
+    }
+
+    public static int minimumDifferenceUtil2(int[] nums, int index, int totalSum, int subset1,  int subset1Size) {
+        if(index==nums.length){
+            if(subset1Size==nums.length/2) return Math.abs(totalSum-2*subset1);
+            else return Integer.MAX_VALUE;
+        }
+        int chooseSubset1 = minimumDifferenceUtil2(nums,index+1,totalSum, subset1+nums[index], subset1Size+1);
+        int chooseSubset2 = minimumDifferenceUtil2(nums,index+1, totalSum,  subset1,  subset1Size);
+        return Math.min(chooseSubset1,chooseSubset2);
+    }
+
     public static void main(String[] args) {
         int[] nums = {3,9,7,3};
         System.out.println(minimumDifference(nums));
+        System.out.println(minimumDifference2(nums));
 
         System.out.println("--------------------------------------------");
 
         int[] nums1 = {-36,36};
         System.out.println(minimumDifference(nums1));
+        System.out.println(minimumDifference2(nums1));
 
         System.out.println("--------------------------------------------");
 
         int[] nums2 = {2,-1,0,4,-2,-9};
         System.out.println(minimumDifference(nums2));
+        System.out.println(minimumDifference2(nums2));
+
     }
 }
--- AZStriverPlaylist/DynamicProgramming/DPOnSubsequences/PartitionEqualSubsetSum.java
+++ AZStriverPlaylist/DynamicProgramming/DPOnSubsequences/PartitionEqualSubsetSum.java
@@ -21,6 +21,20 @@ public class PartitionEqualSubsetSum {
         return chooseSubset1 || chooseSubset2;
     }
 
+    public static boolean canPartition3(int[] nums) {
+        int totalSum = Arrays.stream(nums).sum();
+        return canPartitionUtil3(nums,0, totalSum, 0);
+    }
+
+    public static boolean canPartitionUtil3(int[] nums, int index, int totalSum, int subset1) {
+        if(index==nums.length){
+            return (totalSum==2*subset1);
+        }
+        boolean chooseSubset1 = canPartitionUtil3(nums,index+1, totalSum, subset1+nums[index]);
+        boolean chooseSubset2 = canPartitionUtil3(nums,index+1, totalSum, subset1);
+        return chooseSubset1 || chooseSubset2;
+    }
+
     public static boolean canPartition2(int[] nums) {
         int totalSum = Arrays.stream(nums).sum();
         if(totalSum%2!=0) return false;
@@ -48,6 +62,7 @@ public class PartitionEqualSubsetSum {
     public static void main(String[] args) {
         int[] nums = {1,5,11,5};
         System.out.println(canPartition(nums));
+        System.out.println(canPartition3(nums));
         System.out.println(canPartition2(nums));
         System.out.println(canPartitionMemoization(nums));
         System.out.println(canPartitionTabulation(nums));
@@ -57,6 +72,7 @@ public class PartitionEqualSubsetSum {
 
         int[] nums1 = {1,2,3,5};
         System.out.println(canPartition(nums1));
+        System.out.println(canPartition3(nums1));
         System.out.println(canPartition2(nums1));
         System.out.println(canPartitionMemoization(nums1));
         System.out.println(canPartitionTabulation(nums1));
