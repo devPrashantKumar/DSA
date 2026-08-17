26b2c6eafb335a7e2ae26dd5afc8cb5ae66e5100
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-08-17 12:46:07 +0530

    feat: refactor BubbleSort implementation and consolidate recursive logic into BubbleSortClass

--- AZStriverPlaylist/SortingAlgorithms/BubbleSort/BubbleSortClass.java
+++ AZStriverPlaylist/SortingAlgorithms/BubbleSort/BubbleSortClass.java
@@ -4,33 +4,63 @@ import java.util.*;
 
 public class BubbleSortClass {
     /*
-     * Time Complexity 
+     * Time Complexity
      * Best Case : O(n) - only one iteration will happen
      * Average/Worst Case : O(n^2)
      * Space Complexity O(1)
      * In-Place Algorithm
      * Stable Algorithm
      */
-    public static int[] bubbleSort(int[] arr){
+    public static int[] bubbleSort(int[] arr) {
         int n = arr.length;
-        for(int i=n-1;i>0;i--){
-            boolean didSwap=false;
-            for(int j=0;j<i;j++){
-                if(arr[j]>arr[j+1]){
+        for (int i = n - 1; i > 0; i--) {
+            boolean didSwap = false;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
-                    didSwap=true;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    didSwap = true;
                 }
             }
-            if(!didSwap) break;
+            if (!didSwap)
+                break;
         }
         return arr;
     }
 
+    /*
+     * Time Complexity : O(n^2)
+     * Space Complexity : O(n) recursive stack
+     */
+    public static void recursiveBubbleSort(int[] arr) {
+        recursiveBubbleSortUtil(arr, arr.length);
+    }
+
+    public static void recursiveBubbleSortUtil(int[] arr, int n) {
+        if (n == 1)
+            return;
+        boolean didSwap=false;
+        for (int j = 0; j < n - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                didSwap = true;
+            }
+        }
+        if(didSwap) recursiveBubbleSortUtil(arr, n - 1);
+    }
+
     public static void main(String[] args) {
-        int[] arr = {2,3,1,7,3,9,6};
-        BubbleSortClass.bubbleSort(arr);
+        int[] arr = { 2, 3, 1, 7, 3, 9, 6 };
+        bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
+
+        System.out.println("---------------------------------");
+
+        int[] arr1 = { 2, 3, 1, 7, 3, 9, 6 };
+        recursiveBubbleSort(arr1);
+        System.out.println(Arrays.toString(arr1));
     }
 }
