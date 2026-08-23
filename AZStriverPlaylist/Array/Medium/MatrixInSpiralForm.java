d7b8a994f1ec4f1e46e901c7e346d955ad51291a
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-08-23 21:44:23 +0530

    feat: implement second method for spiral matrix traversal with comprehensive test cases

--- AZStriverPlaylist/Array/Medium/MatrixInSpiralForm.java
+++ AZStriverPlaylist/Array/Medium/MatrixInSpiralForm.java
@@ -39,6 +39,41 @@ public class MatrixInSpiralForm {
         return list;
     }
 
+    public static List<Integer> spiralFormMatrix_2(int[][] matrix) {
+        List<Integer> list = new ArrayList<>();
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int top = 0;
+        int bottom = n - 1;
+        int left = 0;
+        int right = m - 1;
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) {
+                list.add(matrix[top][i]);
+            }
+            for (int i = top + 1; i <= bottom; i++) {
+                list.add(matrix[i][right]);
+            }
+            if (top + 1 <= bottom) {
+                for (int i = right - 1; i >= left; i--) {
+                    list.add(matrix[bottom][i]);
+                }
+
+                if (right - 1 >= left) {
+                    for (int i = bottom - 1; i >= top + 1; i--) {
+                        list.add(matrix[i][left]);
+                    }
+                }
+            }
+            
+            left++;
+            right--;
+            top++;
+            bottom--;
+        }
+        return list;
+    }
+
     public static void main(String[] args) {
         int[][] input1 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
         System.out.println(
@@ -52,5 +87,18 @@ public class MatrixInSpiralForm {
         System.out.println(
                 "Input : " + Arrays.deepToString(input3) + " Output : " + MatrixInSpiralForm.spiralFormMatrix(input3));
 
+        System.out.println("------------------------------------------------------------");
+
+        int[][] input11 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        System.out.println(
+                "Input : " + Arrays.deepToString(input11) + " Output : " + MatrixInSpiralForm.spiralFormMatrix_2(input11));
+
+        int[][] input21 = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
+        System.out.println(
+                "Input : " + Arrays.deepToString(input21) + " Output : " + MatrixInSpiralForm.spiralFormMatrix_2(input21));
+
+        int[][] input31 = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+        System.out.println(
+                "Input : " + Arrays.deepToString(input31) + " Output : " + MatrixInSpiralForm.spiralFormMatrix_2(input31));
     }
 }
