180de6119544ffe5c90223a804c42d8b6bf03144
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-08-25 11:44:46 +0530

    feat: add MergeTwoSortedArraysWithoutExtraSpaceII and MergeTwoSortedArraysWithoutExtraSpaceIII implementations with test cases

--- AZStriverPlaylist/Array/Hard/MergeTwoSortedArraysWithoutExtraSpaceII.java
+++ AZStriverPlaylist/Array/Hard/MergeTwoSortedArraysWithoutExtraSpaceII.java
@@ -2,31 +2,31 @@ package AZStriverPlaylist.Array.Hard;
 
 import java.util.Arrays;
 
-public class MergeTwoSortedArraysWithoutExtraSpace {
+public class MergeTwoSortedArraysWithoutExtraSpaceII {
 
-    // TC : O(m+n)
+    // TC : O(min(m,n))+O(nlogn)+O(mlogm)
+    // SC : O(1)
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int index=m+n-1;
-        while(m>0 && n>0){
-            if(nums1[m-1]>nums2[n-1]){
-                nums1[index] = nums1[m-1];
-                m--;
-            }else{
-                nums1[index] = nums2[n-1];
-                n--;
-            }
-            index--;
-        }
+        int leftLargest = m-1;
+        int rightSmallest = 0;
 
-        while(n>0){
-            nums1[index] = nums2[n-1];
-            n--;
-            index--;
+        while (leftLargest>=0 && rightSmallest<n) {
+            if(nums1[leftLargest]>nums2[rightSmallest]) {
+                int temp = nums1[leftLargest];
+                nums1[leftLargest] = nums2[rightSmallest];
+                nums2[rightSmallest] = temp;
+            }else{
+                break;
+            }
+            leftLargest--;
+            rightSmallest++;
         }
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
     }
 
     public static void main(String[] args) {
-        int[] nums1 = { 1,2,3,0,0,0 };
+        int[] nums1 = { 1,2,3 };
         int m = 3;
         int[] nums2 = { 2,5,6 };
         int n = 3;
@@ -34,7 +34,7 @@ public class MergeTwoSortedArraysWithoutExtraSpace {
         System.out.println("Input : m -> "+ m + " -> " + Arrays.toString(nums1));
         System.out.println("Input : m -> "+ n + " -> " + Arrays.toString(nums2));
         merge(nums1,m, nums2, n);
-        System.out.println("Output : " + Arrays.toString(nums1));
+        System.out.println("Output : nums1->" + Arrays.toString(nums1)+" nums2->" + Arrays.toString(nums2));
 
         System.out.println("---------------------------------------------------");
 
@@ -46,11 +46,11 @@ public class MergeTwoSortedArraysWithoutExtraSpace {
         System.out.println("Input : m -> "+ m1 + " -> " + Arrays.toString(nums11));
         System.out.println("Input : m -> "+ n1 + " -> " + Arrays.toString(nums12));
         merge(nums11,m1, nums12, n1);
-        System.out.println("Output : " + Arrays.toString(nums11));
+        System.out.println("Output : " + Arrays.toString(nums11)+" nums2->" + Arrays.toString(nums12));
 
         System.out.println("---------------------------------------------------");
 
-        int[] nums21 = { 0 };
+        int[] nums21 = { };
         int m2 = 0;
         int[] nums22 = { 1 };
         int n2 = 1;
@@ -58,7 +58,7 @@ public class MergeTwoSortedArraysWithoutExtraSpace {
         System.out.println("Input : m -> "+ m2 + " -> " + Arrays.toString(nums21));
         System.out.println("Input : m -> "+ n2 + " -> " + Arrays.toString(nums22));
         merge(nums21,m2, nums22, n2);
-        System.out.println("Output : " + Arrays.toString(nums21));
+        System.out.println("Output : " + Arrays.toString(nums21)+" nums2->" + Arrays.toString(nums22));
        
     }
 }
---
+++ AZStriverPlaylist/Array/Hard/MergeTwoSortedArraysWithoutExtraSpaceIII.java
@@ -0,0 +1,78 @@
+package AZStriverPlaylist.Array.Hard;
+
+import java.util.Arrays;
+
+public class MergeTwoSortedArraysWithoutExtraSpaceIII {
+
+    // using Gap aaproach (inspired from shell sort)
+    public static void mergeOptimal(int[] nums1, int m, int[] nums2, int n) {
+        int len = m+n;
+        int gap = (len+1)/2;
+
+        while(gap>0){
+            int left=0;
+            int right = left+gap;
+            while(right<len){
+                if(left<m && right>=m){
+                    swapIfGreater(nums1, nums2, left, right-m);
+                }
+                else if(left>=m){
+                    swapIfGreater(nums2, nums2, left-m, right-m);
+
+                }else{
+                    swapIfGreater(nums1, nums1, left, right);
+                }
+                left++;right++;
+            }
+            if(gap==1) break;
+            gap = (gap/2)+(gap%2);
+        }
+
+        
+    }
+
+    private static void swapIfGreater(int[] nums1, int[] nums2, int ind1, int ind2){
+        if(nums1[ind1]>nums2[ind2]){
+            int temp = nums1[ind1];
+            nums1[ind1] = nums2[ind2];
+            nums2[ind2] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = { 1,2,3 };
+        int m = 3;
+        int[] nums2 = { 2,5,6 };
+        int n = 3;
+        System.out.println("---------------------------------------------------");
+        System.out.println("Input : m -> "+ m + " -> " + Arrays.toString(nums1));
+        System.out.println("Input : m -> "+ n + " -> " + Arrays.toString(nums2));
+        mergeOptimal(nums1,m, nums2, n);
+        System.out.println("Output : nums1->" + Arrays.toString(nums1)+" nums2->" + Arrays.toString(nums2));
+
+        System.out.println("---------------------------------------------------");
+
+        int[] nums11 = { 1 };
+        int m1 = 1;
+        int[] nums12 = { };
+        int n1 = 0;
+        System.out.println("---------------------------------------------------");
+        System.out.println("Input : m -> "+ m1 + " -> " + Arrays.toString(nums11));
+        System.out.println("Input : m -> "+ n1 + " -> " + Arrays.toString(nums12));
+        mergeOptimal(nums11,m1, nums12, n1);
+        System.out.println("Output : " + Arrays.toString(nums11)+" nums2->" + Arrays.toString(nums12));
+
+        System.out.println("---------------------------------------------------");
+
+        int[] nums21 = { };
+        int m2 = 0;
+        int[] nums22 = { 1 };
+        int n2 = 1;
+        System.out.println("---------------------------------------------------");
+        System.out.println("Input : m -> "+ m2 + " -> " + Arrays.toString(nums21));
+        System.out.println("Input : m -> "+ n2 + " -> " + Arrays.toString(nums22));
+        mergeOptimal(nums21,m2, nums22, n2);
+        System.out.println("Output : " + Arrays.toString(nums21)+" nums2->" + Arrays.toString(nums22));
+       
+    }
+}
