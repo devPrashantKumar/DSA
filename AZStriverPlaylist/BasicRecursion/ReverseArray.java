07fc0129b9fc54f5cf56d682cf8f3010c438fb04
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-08-27 15:37:23 +0530

    feat: add third method for reversing an array using recursion

--- AZStriverPlaylist/BasicRecursion/ReverseArray.java
+++ AZStriverPlaylist/BasicRecursion/ReverseArray.java
@@ -20,6 +20,17 @@ public class ReverseArray {
         arr[arr.length-1-(len-1)] = element;
     }
 
+    public static void reverseArrayUsingRecursion3(int[] arr, int len){
+        reverseArrayUsingRecursion3Util(arr, len, 0);
+    }
+
+    public static void reverseArrayUsingRecursion3Util(int[] arr, int len, int index){
+        if(len==index) return;
+        int element = arr[index];
+        reverseArrayUsingRecursion3Util(arr, len, index+1);
+        arr[len-1-(index)] = element;
+    }
+
     public static void main(String[] args) {
         int[] input1={1,2,3,4,5,6};
         System.out.println("Input 1 " + Arrays.toString(input1));
@@ -32,5 +43,12 @@ public class ReverseArray {
         System.out.println("Input 2 " + Arrays.toString(input2));
         reverseArrayUsingRecursion2(input2,0,input2.length-1);
         System.out.println("Output 2 "+Arrays.toString(input2));
+
+        System.out.println("----------------------------------------");
+
+        int[] input3={1,2,3,4,5,6};
+        System.out.println("Input 2 " + Arrays.toString(input3));
+        reverseArrayUsingRecursion3(input3,input3.length);
+        System.out.println("Output 2 "+Arrays.toString(input3));
     }
 }
