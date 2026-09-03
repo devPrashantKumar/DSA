f325c14773912766fc491301f1aec47cfdb1212e
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-09-03 14:48:38 +0530

    feat: add findKthPositiveUsingBS2 method for enhanced binary search functionality

--- AZStriverPlaylist/BinarySearch/BSOnAnswers/KthMissingPositiveNumber.java
+++ AZStriverPlaylist/BinarySearch/BSOnAnswers/KthMissingPositiveNumber.java
@@ -29,11 +29,30 @@ public class KthMissingPositiveNumber {
         //return left+k; simplified version of above formula
     }
 
+    public static int findKthPositiveUsingBS2(int[] arr, int k) {
+        int left=0;
+        int right = arr.length-1;
+        int ans = -1;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            if(arr[mid] - (mid + 1) >= k) right = mid-1;
+            else {
+                ans = mid;
+                left = mid+1;
+            }
+        }
+        return ans>=0 ? arr[ans]+(k-(arr[ans]-(ans+1))) : k;
+        //return left+k; simplified version of above formula
+    }
+
     public static void main(String[] args) {
         System.out.println(findKthPositive(new int[] { 2,3,4,7,11 }, 5));
-        System.out.println(findKthPositive(new int[] { 1,2,3,4 }, 2));
-        System.out.println("===================================");
         System.out.println(findKthPositiveUsingBS(new int[] { 2,3,4,7,11 }, 5));
+        System.out.println(findKthPositiveUsingBS2(new int[] { 2,3,4,7,11 }, 5));
+
+        System.out.println("===================================");
+        System.out.println(findKthPositive(new int[] { 1,2,3,4 }, 2));
         System.out.println(findKthPositiveUsingBS(new int[] { 1,2,3,4 }, 2));
+        System.out.println(findKthPositiveUsingBS2(new int[] { 1,2,3,4 }, 2));
     }
 }
