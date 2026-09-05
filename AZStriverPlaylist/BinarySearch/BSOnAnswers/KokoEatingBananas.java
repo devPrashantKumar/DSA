856b8d8cf4daf93ad26a5b20822e11dcf45ae44d
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-09-06 03:17:45 +0530

    fix: correct condition in minEatingSpeed for accurate binary search logic

--- AZStriverPlaylist/BinarySearch/BSOnAnswers/KokoEatingBananas.java
+++ AZStriverPlaylist/BinarySearch/BSOnAnswers/KokoEatingBananas.java
@@ -18,11 +18,11 @@ public class KokoEatingBananas {
         // int ans = -1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (!calculateHours(mid, h, piles)) {
-                left = mid + 1;
-            } else {
+            if (calculateHours(mid, h, piles)) {
                 // ans = mid;
                 right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
         return left;
