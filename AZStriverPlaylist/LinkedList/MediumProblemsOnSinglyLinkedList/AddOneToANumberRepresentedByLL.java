3736192b08c8471fb3a98aaccdd94474397f2aec
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-09-14 19:31:23 +0530

    feat: implement addTwoNumbersInLLUsingDummyNode method for adding two numbers represented by linked lists

--- AZStriverPlaylist/LinkedList/MediumProblemsOnSinglyLinkedList/AddOneToANumberRepresentedByLL.java
+++ AZStriverPlaylist/LinkedList/MediumProblemsOnSinglyLinkedList/AddOneToANumberRepresentedByLL.java
@@ -1,6 +1,10 @@
 package AZStriverPlaylist.LinkedList.MediumProblemsOnSinglyLinkedList;
 
 public class AddOneToANumberRepresentedByLL {
+    /* 
+    * Time Complexity : O(n) 
+    * Space Complexity : O(n), recursion stack space
+    */
     public static SingleLinkedListNode<Integer> addOneToLL(SingleLinkedListNode<Integer> head){
         int carry = addOneToLLUtil(head);
         if(carry!=0){
@@ -19,19 +23,65 @@ public class AddOneToANumberRepresentedByLL {
         }
         return carry;
     }
+
+    /* 
+    * Time Complexity : O(n) 
+    * Space Complexity : O(1)
+    */
+    public static SingleLinkedListNode<Integer> addOneToLL2(SingleLinkedListNode<Integer> head){
+        int carry = 1;
+        if(head==null) return  new SingleLinkedListNode<Integer>(carry);
+
+        head = reverseLL(head);
+
+        SingleLinkedListNode<Integer> prev = null;
+        SingleLinkedListNode<Integer> curr = head;
+        while(curr!=null && carry != 0){
+            int data = carry+curr.data;
+            curr.data = data%10;
+            carry = data/10;
+            prev = curr;
+            curr = curr.next;
+        }
+        if(carry!=0){
+            prev.next = new SingleLinkedListNode<Integer>(carry);
+        }
+        return reverseLL(head);
+    }
+
+    public static SingleLinkedListNode<Integer> reverseLL(SingleLinkedListNode<Integer> head){
+        SingleLinkedListNode<Integer> prev = null;
+        SingleLinkedListNode<Integer> curr =head;
+        while(curr!=null){
+            SingleLinkedListNode<Integer> currNext = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = currNext;
+        }
+        return prev;
+    }
     public static void main(String[] args) {
-        Integer[] arr1 = {1,2,3,4,5,6,7};
+        Integer[] arr1 = {1,2,3,4,5,6,8};
         SingleLinkedListNode<Integer> head = SingleLinkedListUtility.convertArrayToList(arr1);
         SingleLinkedListUtility.printLinkedList(head);
         
+        System.out.println("----------------------------------------------------------");
         head = AddOneToANumberRepresentedByLL.addOneToLL(head);
         SingleLinkedListUtility.printLinkedList(head);
+        System.out.println("----------------------------------------------------------");
+        head = AddOneToANumberRepresentedByLL.addOneToLL2(head);
+        SingleLinkedListUtility.printLinkedList(head);
+        System.out.println("----------------------------------------------------------");
+
         System.out.println("----------------------------------------------------------");
 
         Integer[] arr2 = {9,9,9,9};
         SingleLinkedListNode<Integer> head2 = SingleLinkedListUtility.convertArrayToList(arr2);
         SingleLinkedListUtility.printLinkedList(head2);
-        
+        System.out.println("----------------------------------------------------------");
+        head2 = AddOneToANumberRepresentedByLL.addOneToLL2(head2);
+        SingleLinkedListUtility.printLinkedList(head2);
+        System.out.println("----------------------------------------------------------");
         head2 = AddOneToANumberRepresentedByLL.addOneToLL(head2);
         SingleLinkedListUtility.printLinkedList(head2);
         System.out.println("----------------------------------------------------------");
--- AZStriverPlaylist/LinkedList/MediumProblemsOnSinglyLinkedList/AddTwoNumbersInLL.java
+++ AZStriverPlaylist/LinkedList/MediumProblemsOnSinglyLinkedList/AddTwoNumbersInLL.java
@@ -47,6 +47,38 @@ public class AddTwoNumbersInLL {
         }
         return finalHead;
     }
+
+    public static SingleLinkedListNode<Integer> addTwoNumbersInLLUsingDummyNode(SingleLinkedListNode<Integer> head1, SingleLinkedListNode<Integer> head2){
+        SingleLinkedListNode<Integer> finalHead = new SingleLinkedListNode<Integer>(null);
+        SingleLinkedListNode<Integer> finalTail = finalHead;
+        int carry=0;
+        while(head1!=null && head2!=null){
+            int data = head1.data+head2.data+carry;
+            carry = data/10;
+            finalTail.next = new SingleLinkedListNode<Integer>(data%10);
+            finalTail = finalTail.next;
+            head1 = head1.next;
+            head2 = head2.next;
+        }
+        while(head1!=null){
+            int data = head1.data+carry;
+            carry = data/10;        
+            finalTail.next = new SingleLinkedListNode<Integer>(data%10);
+            finalTail = finalTail.next;
+            head1 = head1.next;
+        }
+        while(head2!=null){
+            int data = head2.data+carry;
+            carry = data/10;    
+            finalTail.next = new SingleLinkedListNode<Integer>(data%10);
+            finalTail = finalTail.next;
+            head2 = head2.next;
+        }
+        if(carry!=0){
+            finalTail.next = new SingleLinkedListNode<Integer>(carry);
+        }
+        return finalHead.next;
+    }
     public static void main(String[] args) {
         Integer[] arr1 = {1,2,3,4,5,6,7};
         SingleLinkedListNode<Integer> head1 = SingleLinkedListUtility.convertArrayToList(arr1);
@@ -54,9 +86,13 @@ public class AddTwoNumbersInLL {
         SingleLinkedListNode<Integer> head2 = SingleLinkedListUtility.convertArrayToList(arr2);
         SingleLinkedListUtility.printLinkedList(head1);
         SingleLinkedListUtility.printLinkedList(head2);
-
+        System.out.println("---------------------------------------------------------");
         SingleLinkedListNode<Integer> head3 = AddTwoNumbersInLL.addTwoNumbersInLL(head1,head2);
         SingleLinkedListUtility.printLinkedList(head3);
+        System.out.println("----------------------------------------------------------");
+        System.out.println("---------------------------------------------------------");
+        SingleLinkedListNode<Integer> head4 = AddTwoNumbersInLL.addTwoNumbersInLLUsingDummyNode(head1,head2);
+        SingleLinkedListUtility.printLinkedList(head4);
         System.out.println("----------------------------------------------------------");
     }
 }
