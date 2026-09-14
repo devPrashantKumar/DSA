9f67521e62b82cd3e9c2f7ac656070b54574ae85
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-09-14 18:31:09 +0530

    feat: refactor sorting methods to use merge sort and implement merge2 for improved efficiency

--- AZStriverPlaylist/LinkedList/MediumProblemsOnSinglyLinkedList/SortList.java
+++ AZStriverPlaylist/LinkedList/MediumProblemsOnSinglyLinkedList/SortList.java
@@ -42,16 +42,16 @@ public class SortList {
         SingleLinkedListNode<Integer> firstHead = head;
         SingleLinkedListNode<Integer> secondHead = middle.next;
         middle.next=null;
-        firstHead = sortList(firstHead);
-        secondHead = sortList(secondHead);
-        return merge(firstHead,secondHead);
+        firstHead = sortListUsingMerge(firstHead);
+        secondHead = sortListUsingMerge(secondHead);
+        return merge2(firstHead,secondHead);
     }
 
     public static SingleLinkedListNode<Integer> findMiddle(SingleLinkedListNode<Integer> head) {
         if(head==null || head.next==null) return head;
         SingleLinkedListNode<Integer> tortoise = head;
         SingleLinkedListNode<Integer> hare = head;
-        hare = hare.next;
+        hare = hare.next;  // this line is important
         while(hare!=null && hare.next!=null){
             tortoise = tortoise.next;
             hare = hare.next.next;
@@ -82,6 +82,28 @@ public class SortList {
             tail.next = head2;
             tail = tail.next;
             head2 = head2.next;
+        }
+        return head.next;
+    }
+
+    public static SingleLinkedListNode<Integer> merge2(SingleLinkedListNode<Integer> head1, SingleLinkedListNode<Integer> head2) {
+        SingleLinkedListNode<Integer> head = new SingleLinkedListNode<Integer>(-1);
+        SingleLinkedListNode<Integer> tail = head;
+        while (head1 != null && head2 != null) {
+            if (head1.data <= head2.data) {
+                tail.next = head1;
+                head1 = head1.next;
+            } else {
+                tail.next = head2;
+                head2 = head2.next;
+            }
+            tail = tail.next;
+        }
+        if(head1 != null) {
+            tail.next = head1;
+        }
+        else{
+            tail.next = head2;
         }
         return head.next;
     }
