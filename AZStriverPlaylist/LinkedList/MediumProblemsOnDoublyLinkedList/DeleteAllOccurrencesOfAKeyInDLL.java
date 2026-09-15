b6697f94379347cba45e5ac1540a9e2dfa4cd85e
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-09-15 12:05:45 +0530

    feat: implement deleteAllOccurrences2 method for enhanced deletion of target nodes in doubly linked lists

--- AZStriverPlaylist/LinkedList/MediumProblemsOnDoublyLinkedList/DeleteAllOccurrencesOfAKeyInDLL.java
+++ AZStriverPlaylist/LinkedList/MediumProblemsOnDoublyLinkedList/DeleteAllOccurrencesOfAKeyInDLL.java
@@ -17,11 +17,31 @@ public class DeleteAllOccurrencesOfAKeyInDLL {
         DoubleLinkedListNode<T> curr = head;
         while(curr!=null){
             if(curr.data.equals(target)){
-                if(curr.prev!=null){
-                    curr.prev.next = curr.next;
-                }
+                curr.prev.next = curr.next;
                 if(curr.next!=null){
                     curr.next.prev = curr.prev;
+                }
+            }
+            curr = curr.next;
+        }
+        return head;
+    }
+
+    public static <T> DoubleLinkedListNode<T> deleteAllOccurrences2(DoubleLinkedListNode<T> head, T target){
+        DoubleLinkedListNode<T> curr = head;
+        while(curr!=null){
+            if(curr.data.equals(target)){
+                if(curr.prev==null){
+                    head = curr.next;
+                    if (head != null) {
+                        head.prev = null;
+                    }
+                }
+                else{
+                    curr.prev.next = curr.next;
+                    if(curr.next!=null){
+                        curr.next.prev = curr.prev;
+                    }
                 }
             }
             curr = curr.next;
@@ -50,6 +70,31 @@ public class DeleteAllOccurrencesOfAKeyInDLL {
         head2 = DeleteAllOccurrencesOfAKeyInDLL.deleteAllOccurrences(head2,target2);
         DoubleLinkedListUtility.printDoubleLinkedList(head2);
         System.out.println("----------------------------------------------------------");
+
+        System.out.println("----------------------------------------------------------");
+        System.out.println("----------------------------------------------------------");
+
+        Integer[] arr3 = {1,2,3,1,4};
+        Integer target3 = 1;
+        DoubleLinkedListNode<Integer> head3 = DoubleLinkedListUtility.convertArrayToList(arr3);
+        DoubleLinkedListUtility.printDoubleLinkedList(head3);
+        System.out.println("----------------------------------------------------------");
+
+        head3 = DeleteAllOccurrencesOfAKeyInDLL.deleteAllOccurrences2(head3,target3);
+        DoubleLinkedListUtility.printDoubleLinkedList(head3);
+        System.out.println("----------------------------------------------------------");
+        System.out.println("----------------------------------------------------------");
+        
+        Integer[] arr4 = {2,3,1,4,2};
+        Integer target4 = 2;
+        DoubleLinkedListNode<Integer> head4 = DoubleLinkedListUtility.convertArrayToList(arr4);
+        DoubleLinkedListUtility.printDoubleLinkedList(head4);
+        System.out.println("----------------------------------------------------------");
+
+        head4 = DeleteAllOccurrencesOfAKeyInDLL.deleteAllOccurrences2(head4,target4);
+        DoubleLinkedListUtility.printDoubleLinkedList(head4);
+        System.out.println("----------------------------------------------------------");
+
 
     }
 }
