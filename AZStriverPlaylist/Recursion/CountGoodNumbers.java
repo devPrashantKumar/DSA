075e500184f320611dc464b755651e4391ebbb26
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-09-18 18:36:12 +0530

    feat: implement powXNOptimized2 method for efficient power calculation and update main method for testing

--- AZStriverPlaylist/Recursion/CountGoodNumbers.java
+++ AZStriverPlaylist/Recursion/CountGoodNumbers.java
@@ -7,14 +7,30 @@ public class CountGoodNumbers {
      * Time Complexity : O(log2n)
      */
     public static int countGoodNumbers(long n){
-        long result = ((pow(5,(n+1)/2)*pow(4,n/2))%CountGoodNumbers.MOD);
+        long result = ((pow2(5,(n+1)/2)*pow2(4,n/2))%MOD);
         return (int) result;
     }
 
     public static long pow(long num, long power){
         if(power==0) return 1;
-        if(power%2==0) return pow((num*num)%CountGoodNumbers.MOD,power/2);
-        else return (num*pow(num,power-1))%CountGoodNumbers.MOD;
+        if(power%2==0) return pow((num*num)%MOD,power/2);
+        else return (num*pow(num,power-1))%MOD;
+    }
+
+    public static long pow2(long num, long power){
+        long result=1;
+        while(power>0){
+            if(power%2==0){
+                num = (num*num)%MOD;
+                power /=2;
+            } 
+            else{
+                result = (result*num)%MOD;
+                power -= 1;
+            }
+        }
+        return result;
+        
     }
 
     public static void main(String[] args) {
--- AZStriverPlaylist/Recursion/PowXandN.java
+++ AZStriverPlaylist/Recursion/PowXandN.java
@@ -36,20 +36,44 @@ public class PowXandN {
         else return x*powerOfXN(x, n-1);
     }
 
+    public static double powXNOptimized2(double x, int n){
+        double result=1.0;
+        long nn = n;
+        if(nn<0) nn = -1*nn;
+        if(x==0 || x==1) return x;
+        while(nn>0){
+            if(nn%2!=0){
+                result *= x;
+                nn=nn-1;
+            }else{
+                nn /= 2;
+                x = x*x;
+            }
+        }
+        return (n>0)?  result:1/result;
+    }
+
     public static void main(String[] args) {
         double x1=2.0;
         int n1 = 10;
         System.out.println("Input 1 " + x1+", " + n1 + " Output : "+PowXandN.powXN(x1,n1));
         System.out.println("Input 1 " + x1+", " + n1 + " Output : "+PowXandN.powXNOptimized(x1,n1));
-
+        System.out.println("Input 1 " + x1+", " + n1 + " Output : "+PowXandN.powXNOptimized2(x1,n1));
+        System.out.println("---------------------------------------------");
+        
         double x2=2.10;
         int n2 = 3;
         System.out.println("Input 1 " + x2+", " + n2 + " Output : "+PowXandN.powXN(x2,n2));
         System.out.println("Input 1 " + x2+", " + n2 + " Output : "+PowXandN.powXNOptimized(x2,n2));
+        System.out.println("Input 1 " + x2+", " + n2 + " Output : "+PowXandN.powXNOptimized2(x2,n2));
+        System.out.println("---------------------------------------------");
 
         double x3=2.0;
         int n3 = -2;
         System.out.println("Input 1 " + x3+", " + n3 + " Output : "+PowXandN.powXN(x3,n3));
         System.out.println("Input 1 " + x3+", " + n3 + " Output : "+PowXandN.powXNOptimized(x3,n3));
+        System.out.println("Input 1 " + x3+", " + n3 + " Output : "+PowXandN.powXNOptimized2(x3,n3));
+        System.out.println("---------------------------------------------");
+
     }
 }
