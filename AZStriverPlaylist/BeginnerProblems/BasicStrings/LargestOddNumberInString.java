e501f4eb82f133332382af7d866d7f5d6786d059
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-09-21 12:21:56 +0530

    feat: update LargestOddNumberInString to handle leading zeros and improve output logic

--- AZStriverPlaylist/BeginnerProblems/BasicStrings/LargestOddNumberInString.java
+++ AZStriverPlaylist/BeginnerProblems/BasicStrings/LargestOddNumberInString.java
@@ -6,19 +6,23 @@ public class LargestOddNumberInString {
      * Time Complexity : O(n)
      */
     public static String largestOddNumber(String num) {
+        int start = 0;
+        while(num.charAt(start)=='0') start++;
         int i=num.length()-1;
-        for(;i>=0;i--){
+
+        for(;i>=start;i--){
             if(Character.getNumericValue(num.charAt(i))%2!=0) break;
         }
-        return num.substring(0,i+1);
+        
+        return num.substring(start,i+1);
     }
 
     public static void main(String[] args) {
-        String input1 = "52";
-        System.out.println("Input : "+input1+" Output : "+LargestOddNumberInString.largestOddNumber(input1) );
+        String input1 = "00052";
+        System.out.println("Input : "+input1+" Output : "+largestOddNumber(input1) );
         String input2 = "4206";
-        System.out.println("Input : "+input2+" Output : "+LargestOddNumberInString.largestOddNumber(input2) );
+        System.out.println("Input : "+input2+" Output : "+largestOddNumber(input2) );
         String input3 = "35427";
-        System.out.println("Input : "+input3+" Output : "+LargestOddNumberInString.largestOddNumber(input3) );
+        System.out.println("Input : "+input3+" Output : "+largestOddNumber(input3) );
     }
 }
