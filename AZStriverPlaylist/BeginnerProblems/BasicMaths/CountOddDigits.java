5b7eeb01200586871f1badf954fa8132ce3d98c0
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-09-21 08:31:25 +0530

    feat: implement CountOddDigits class with methods to count odd digits

--- AZStriverPlaylist/BeginnerProblems/BasicMaths/CountOddDigits.java
+++ AZStriverPlaylist/BeginnerProblems/BasicMaths/CountOddDigits.java
@@ -1,1 +1,35 @@
- 
+package AZStriverPlaylist.BeginnerProblems.BasicMaths;
+
+public class CountOddDigits{
+    /*
+     * Time Complexity: O(log10N + 1) 
+     */
+    public static int countNumberOfOddDigits(int num){
+        if(num==0) return 0;
+        int counter=0;
+        while(num>0){
+            int digit = num%10;
+            num /= 10;
+            if(digit%2==1) counter++;
+        }
+        return counter;
+    }
+
+    public static int countNumberOfOddDigitsUsingRecursion(int num){
+        if(num<=0) return 0;
+        int digit = num%10;
+        if(digit%2==1) return 1+countNumberOfOddDigitsUsingRecursion(num/10);
+        else return countNumberOfOddDigitsUsingRecursion(num/10);
+    }
+
+    public static void main(String[] args) {
+        int input1 = 8721648;
+        System.out.println("Input : "+ input1+" Output : "+countNumberOfOddDigits(input1));
+        System.out.println("Input : "+ input1+" Output : "+countNumberOfOddDigitsUsingRecursion(input1));
+
+        System.out.println("-------------------------------------------------------------------------------");
+        int input2 = 0;
+        System.out.println("Input : "+ input2+" Output : "+countNumberOfOddDigits(input2));
+        System.out.println("Input : "+ input2+" Output : "+countNumberOfOddDigitsUsingRecursion(input2));
+    }
+}
