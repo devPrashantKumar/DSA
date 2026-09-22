c8a6a80b20c4cd00cf6f9a4e114de779e5d71aa3
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-09-22 18:39:27 +0530

    feat: add third approach for combination sum and update main method outputs

--- AZStriverPlaylist/Recursion/MediumProblems/CombinationSumII.java
+++ AZStriverPlaylist/Recursion/MediumProblems/CombinationSumII.java
@@ -91,36 +91,57 @@ public class CombinationSumII {
         combination.remove(combination.size()-1);
     }
 
+    public static List<List<Integer>> combinationSumApproach3(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> combination = new ArrayList<>();
+        Arrays.sort(candidates);
+        generateCombinationApproach3(candidates,target,combination,0,-1,result);
+        return result;
+    }
+
+    public static void generateCombinationApproach3(int[] candidates, int target,List<Integer> combination, int index,int lastSkipped,List<List<Integer>> result){
+        if(target<0) return;
+        if(index == candidates.length && target==0){
+            result.add(new ArrayList<>(combination));
+            return;
+        }
+        if(index>=candidates.length) return;
+
+        
+        generateCombinationApproach3(candidates, target, combination, index+1,candidates[index],result);
+        if(lastSkipped!=candidates[index]){
+            combination.add(candidates[index]);
+            generateCombinationApproach3(candidates, target-candidates[index], combination, index+1,lastSkipped,result);
+            combination.remove(combination.size()-1);
+        }
+    }
+
     public static void main(String[] args) {
         int[] candidates1 = {10,1,2,7,6,1,5};
         int target1 = 8;
         System.out.println("Input: " + Arrays.toString(candidates1)+" target: "+target1);
         System.out.println("Output : "+CombinationSumII.combinationSum(candidates1, target1));
+        System.out.println("Output : "+CombinationSumII.combinationSum2(candidates1, target1));
+        System.out.println("Output : "+CombinationSumII.combinationSumApproach2(candidates1, target1));
+        System.out.println("Output : "+CombinationSumII.combinationSumApproach3(candidates1, target1));
+        System.out.println("--------------------------------------------------------------------");
 
         int[] candidates2 = {2,5,2,1,2};
         int target2 = 5;
         System.out.println("Input: " + Arrays.toString(candidates2)+" target: "+target2);
         System.out.println("Output : "+CombinationSumII.combinationSum(candidates2, target2));
+        System.out.println("Output : "+CombinationSumII.combinationSum2(candidates2, target2));
+        System.out.println("Output : "+CombinationSumII.combinationSumApproach2(candidates2, target2));
+        System.out.println("Output : "+CombinationSumII.combinationSumApproach3(candidates2, target2));
         System.out.println("--------------------------------------------------------------------");
-        int[] candidates3 = {10,1,2,7,6,1,5};
-        int target3 = 8;
+
+        int[] candidates3 = {1,1,2};
+        int target3 = 3;
         System.out.println("Input: " + Arrays.toString(candidates3)+" target: "+target3);
+        System.out.println("Output : "+CombinationSumII.combinationSum(candidates3, target3));
         System.out.println("Output : "+CombinationSumII.combinationSum2(candidates3, target3));
+        System.out.println("Output : "+CombinationSumII.combinationSumApproach2(candidates3, target3));
+        System.out.println("Output : "+CombinationSumII.combinationSumApproach3(candidates3, target3));
 
-        int[] candidates4 = {2,5,2,1,2};
-        int target4 = 5;
-        System.out.println("Input: " + Arrays.toString(candidates4)+" target: "+target4);
-        System.out.println("Output : "+CombinationSumII.combinationSum2(candidates4, target4));
-        System.out.println("--------------------------------------------------------------------");
-        int[] candidates5 = {10,1,2,7,6,1,5};
-        int target5 = 8;
-        System.out.println("Input: " + Arrays.toString(candidates5)+" target: "+target5);
-        System.out.println("Output : "+CombinationSumII.combinationSumApproach2(candidates5, target5));
-
-        int[] candidates6 = {2,5,2,1,2};
-        int target6 = 5;
-        System.out.println("Input: " + Arrays.toString(candidates6)+" target: "+target6);
-        System.out.println("Output : "+CombinationSumII.combinationSumApproach2(candidates6, target6));
-        System.out.println("--------------------------------------------------------------------");
     }
 }
